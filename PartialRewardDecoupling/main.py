d1160f49c5127c20fc6016b606527e8b10e0e87e
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2020-06-11 04:26:00 +0530

    Update main.py

--- PartialRewardDecoupling/main.py
+++ PartialRewardDecoupling/main.py
@@ -6,15 +6,15 @@ from torch.utils.tensorboard import SummaryWriter
 if __name__ == '__main__':
 	env = make_env(scenario_name="simple_spread")
 	ma_controller = MAA2C(env)
-	reward_list,entropy_list,value_loss_list,policy_loss_list,total_loss_list = ma_controller.run(100,300)
+	# reward_list,entropy_list,value_loss_list,policy_loss_list = ma_controller.run(10,300)
+	ma_controller.run(100,300)
 	
-	writer = SummaryWriter('runs/simple_spread_lr_2e-4')
+	# writer = SummaryWriter('runs/simple_spread_lr_2e-4')
 
-	for i in range(len(entropy_list)):
-  		writer.add_scalar('Entropy loss',entropy_list[i],i)
-  		writer.add_scalar('Reward',reward_list[i],i)
-  		writer.add_scalar('Value Loss',value_loss_list[i],i)
-  		writer.add_scalar('Policy Loss',policy_loss_list[i],i)
-  		writer.add_scalar('Total Loss',total_loss_list[i],i)
+	# for i in range(len(ma_controller.agents.entropy_list)):
+ #  		writer.add_scalar('Entropy loss',ma_controller.agents.entropy_list[i],i)
+ #  		writer.add_scalar('Reward',ma_controller.episode_rewards[i],i)
+ #  		writer.add_scalar('Value Loss',ma_controller.agents.value_loss_list[i],i)
+ #  		writer.add_scalar('Policy Loss',ma_controller.agents.policy_loss_list[i],i)
 
 	# tensorboard --logdir=runs
