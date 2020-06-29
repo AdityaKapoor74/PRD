5edb7ef63e820e63309e0726895c29588695e760
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2020-06-30 05:07:18 +0530

    Adding changes

--- SimpleSpread/a2c_agent.py
+++ SimpleSpread/a2c_agent.py
@@ -12,7 +12,7 @@ import gc
 
 class A2CAgent:
 
-  def __init__(self,env,lr=4e-4,gamma=0.99):
+  def __init__(self,env,lr=2e-4,gamma=0.99):
     self.env = env
     self.lr = lr
     self.gamma = gamma
@@ -24,7 +24,8 @@ class A2CAgent:
     self.input_dim = env.observation_space[0].shape[0]
     self.action_dim = self.env.action_space[0].n
     self.actorcritic = CentralizedActorCritic(self.input_dim,self.action_dim).to(self.device)
-#     self.actorcritic.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
+    model_path = "/home/aditya/Desktop/Partial_Reward_Decoupling/PRD/SimpleSpread/models/actorcritic_network_lr_2e-4_with_grad_norm_1_entropy_pen_0.008_xavier_init_clamp_logs.pt"
+    self.actorcritic.load_state_dict(torch.load(model_path,map_location=torch.device('cpu')))
 
     self.MSELoss = nn.MSELoss()
     self.actorcritic_optimizer = optim.Adam(self.actorcritic.parameters(),lr=lr)
@@ -33,16 +34,19 @@ class A2CAgent:
     self.value_loss_list = []
     self.policy_loss_list = []
     self.total_loss_list = []
-    self.writer = SummaryWriter('runs/simple_spread_lr_4e-4_with_grad_norm_100')
+    self.writer = SummaryWriter('runs/simple_spread_lr_2e-4_with_grad_norm_1_entropy_pen_0.008_xavier_init_clamp_logs')
 
   def get_action(self,state):
     state = torch.FloatTensor(state).to(self.device)
     logits,_ = self.actorcritic.forward(state)
+    # print("logits:",logits)
     del state
     dist = F.softmax(logits,dim=0)
+    # print("dist:",dist)
+    del logits
     # print('dist: ', dist)
     probs = Categorical(dist)
-
+    # print("PROBS:",probs)
     index = probs.sample().cpu().detach().item()
     
 
@@ -80,7 +84,7 @@ class A2CAgent:
 
     entropy = []
     for dist in dists:
-      entropy.append(-torch.sum(dist*torch.log(dist)))
+      entropy.append(-torch.sum(dist*torch.log(torch.clamp(dist,1e-10,1))))
     entropy = torch.stack(entropy).mean()
     self.entropy_list.append(entropy.item())
 
@@ -91,11 +95,11 @@ class A2CAgent:
     policy_loss = policy_loss.mean()
     self.policy_loss_list.append(policy_loss.item())
     self.value_loss_list.append(critic_loss.item())
-    total_loss = policy_loss + critic_loss - 0.001*entropy
+    total_loss = policy_loss + critic_loss - 0.008*entropy
     self.total_loss_list.append(total_loss.item())
     self.actorcritic_optimizer.zero_grad()
     total_loss.backward(retain_graph=False)
-    grad_norm = torch.nn.utils.clip_grad_norm_(self.actorcritic.parameters(),100)
+    grad_norm = torch.nn.utils.clip_grad_norm_(self.actorcritic.parameters(),1)
     self.actorcritic_optimizer.step()
     # total_loss.detach()
     
