094cf2fa92735fc01127c05ba105eb4d79784b72
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2020-07-08 14:52:35 +0530

    Update maa2c.py

--- SimpleSpread/MultiAgent/maa2c.py
+++ SimpleSpread/MultiAgent/maa2c.py
@@ -21,7 +21,7 @@ class MAA2C:
     self.agents = A2CAgent(self.env)
 
 
-    self.writer = SummaryWriter('runs/simple_spread_lr_4e-4_with_grad_norm_1_entropy_pen_0.008_xavier_init_clamp_logs_two_agent')
+    # self.writer = SummaryWriter('longer_runs/two_agents/simple_spread_2_shared_layers_no_comms_discounted_rewards_lr_2e-4_with_grad_norm_0.5_entropy_pen_0.008_xavier_uniform_init_clamp_logs_continued')
 
   def get_actions(self,states):
     actions = []
@@ -39,11 +39,11 @@ class MAA2C:
 
     critic_loss,policy_loss,entropy,total_loss,grad_norm = self.agents.update(states,next_states,actions,rewards)
 
-    self.writer.add_scalar('Loss/Entropy loss',entropy,episode)
-    self.writer.add_scalar('Loss/Value Loss',critic_loss,episode)
-    self.writer.add_scalar('Loss/Policy Loss',policy_loss,episode)
-    self.writer.add_scalar('Loss/Total Loss',total_loss,episode)
-    self.writer.add_scalar('Gradient Normalization/Grad Norm',grad_norm,episode)
+    # self.writer.add_scalar('Loss/Entropy loss',entropy,episode)
+    # self.writer.add_scalar('Loss/Value Loss',critic_loss,episode)
+    # self.writer.add_scalar('Loss/Policy Loss',policy_loss,episode)
+    # self.writer.add_scalar('Loss/Total Loss',total_loss,episode)
+    # self.writer.add_scalar('Gradient Normalization/Grad Norm',grad_norm,episode)
 
 
 
@@ -57,18 +57,25 @@ class MAA2C:
         actions = self.get_actions(states)
         next_states,rewards,dones,info = self.env.step(actions)
 
+        # print(rewards)
+
 
         episode_reward += np.sum(rewards)
 
+        # print(dones)
+        # print(all(dones))
+
         if all(dones) or step == max_steps-1:
-          print("STEP",step)
+          # print("STEP",step)
+          # print(all(dones))
+
           dones = [1 for _ in range(self.num_agents)]
           trajectory.append([states,next_states,actions,rewards])
           print("*"*100)
           print("EPISODE: {} | REWARD: {} \n".format(episode,np.round(episode_reward,decimals=4)))
           print("*"*100)
-          self.writer.add_scalar('Reward Incurred/Length of the episode',step,episode)
-          self.writer.add_scalar('Reward Incurred/Reward',episode_reward,episode)
+          # self.writer.add_scalar('Reward Incurred/Length of the episode',step,episode)
+          # self.writer.add_scalar('Reward Incurred/Reward',episode_reward,episode)
           break
         else:
           dones = [0 for _ in range(self.num_agents)]
@@ -76,8 +83,8 @@ class MAA2C:
           states = next_states
       
 #       make a directory called models
-      if episode%500:
-        torch.save(self.agents.actorcritic.state_dict(), "./models/actorcritic_network_lr_4e-4_with_grad_norm_1_entropy_pen_0.008_xavier_init_clamp_logs_two_agent.pt")
+      # if episode%500:
+      #   torch.save(self.agents.actorcritic.state_dict(), "./models/two_agents/actorcritic_network_no_comms_discounted_rewards_lr_2e-4_with_grad_norm_0.5_entropy_pen_0.008_xavier_uniform_init_clamp_logs.pt")
       
         
       self.update(trajectory,episode) 
