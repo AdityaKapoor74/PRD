29109998746153207bfb89ed3bf9ab116b1b5503
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2020-07-15 13:29:48 +0530

    Updating networks

--- PartialRewardDecoupling/a2c.py
+++ PartialRewardDecoupling/a2c.py
@@ -7,12 +7,17 @@ class ValueNetwork(nn.Module):
 
     def __init__(self,input_dim,output_dim):
         super(ValueNetwork,self).__init__()
-        self.fc1 = nn.Linear(input_dim,256)
-        self.fc2 = nn.Linear(256,output_dim)
+        self.fc1 = nn.Linear(input_dim,512)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        self.fc2 = nn.Linear(512,256)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+        self.value = nn.Linear(256,output_dim)
+        torch.nn.init.xavier_uniform_(self.value.weight)
 
     def forward(self,state):
         value = F.relu(self.fc1(state))
-        value = self.fc2(value)
+        value = F.relu(self.fc2(value))
+        value = self.value(value)
 
         return value
 
@@ -21,12 +26,17 @@ class PolicyNetwork(nn.Module):
 
     def __init__(self,input_dim,output_dim):
         super(PolicyNetwork,self).__init__()
-        self.fc1 = nn.Linear(input_dim,256)
-        self.fc2 = nn.Linear(256,output_dim)
+        self.fc1 = nn.Linear(input_dim,512)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        self.fc2 = nn.Linear(512,256)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+        self.policy = nn.Linear(256,output_dim)
+        torch.nn.init.xavier_uniform_(self.policy.weight)
 
     def forward(self,state):
         logits = F.relu(self.fc1(state))
-        logits = self.fc2(logits)
+        logits = F.relu(self.fc2(logits))
+        logits = self.policy(logits)
 
         return logits
 
