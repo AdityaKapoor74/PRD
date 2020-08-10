1c9c249fe7758fbcf0b0a49ae16cf10f19d7c28d
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2020-08-10 11:40:19 +0530

    With action conditioning

--- PartialRewardDecoupling/maa2c.py
+++ PartialRewardDecoupling/maa2c.py
@@ -17,8 +17,7 @@ class MAA2C:
     self.num_agents = env.n
     self.agents = A2CAgent(self.env)
 
-
-    self.writer = SummaryWriter('four_agents/separate_layer_xavier_init_value_lr_2e-4_policy_lr_2e-4_entropy_pen_0.008_cont(4)')
+    self.writer = SummaryWriter('/home/aditya/Desktop/Partial_Reward_Decoupling/git/PRD/PartialRewardDecoupling_new/runs/3_agents/value_lr_2e-4_policy_lr_2e-4_entropy_0.008')
 
   def get_actions(self,states):
     actions = []
@@ -29,47 +28,35 @@ class MAA2C:
 
   def update(self,trajectory,episode):
 
-    states_policy = torch.FloatTensor([sars[0] for sars in trajectory]).to(self.device)
-    actions = torch.LongTensor([sars[1] for sars in trajectory]).to(self.device)
-    rewards = torch.FloatTensor([sars[2] for sars in trajectory])
-    states_value = torch.FloatTensor([sars[3] for sars in trajectory]).to(self.device)
+    states = torch.FloatTensor([sars[0] for sars in trajectory]).to(self.device)
+    next_states = torch.LongTensor([sars[1] for sars in trajectory]).to(self.device)
+    actions = torch.FloatTensor([sars[2] for sars in trajectory]).to(self.device)
+    rewards = torch.FloatTensor([sars[3] for sars in trajectory]).to(self.device)
 
+# ***********************************************************************************
+    value_loss,policy_loss,entropy,grad_norm_value,grad_norm_policy = self.agents.update(states,next_states,actions,rewards)
+    # self.agents.update(states_policy,states_weight,actions,rewards)
 
-    value_loss,policy_loss,entropy,grad_norm_value,grad_norm_policy = self.agents.update(states_policy,actions,rewards,states_value)
 
     self.writer.add_scalar('Loss/Entropy loss',entropy,episode)
     self.writer.add_scalar('Loss/Value Loss',value_loss,episode)
     self.writer.add_scalar('Loss/Policy Loss',policy_loss,episode)
     self.writer.add_scalar('Gradient Normalization/Grad Norm Value',grad_norm_value,episode)
     self.writer.add_scalar('Gradient Normalization/Grad Norm Policy',grad_norm_policy,episode)
-
+# ***********************************************************************************
 
 
 
   def run(self,max_episode,max_steps):  
-    for episode in range(max_episode):
-      states_policy = self.env.reset()
+    for episode in range(164405,max_episode):
+      states = np.asarray(self.env.reset())
+
       trajectory = []
       episode_reward = 0
       for step in range(max_steps):
-        actions = self.get_actions(states_policy)
+
+        actions = self.get_actions(states)
         next_states,rewards,dones,info = self.env.step(actions)
-
-#         states_value = []
-
-#         for i in range(self.num_agents):
-#           actions_copy = np.copy(actions)
-#           actions_copy = np.delete(actions_copy,[i])
-#           tmp = np.copy(states_policy[i])
-#           for j in range(self.num_agents-1):
-#             if j==self.num_agents-2:
-#               tmp = np.append(tmp,actions_copy[j])
-#             else:
-#               tmp = np.insert(tmp,-(self.num_agents-2-j),actions_copy[j])
-#           # states_value.append(np.append(states_policy[i],actions_copy))
-#           states_value.append(tmp)
-
-#         states_value = np.asarray(states_value)
 
         episode_reward += np.sum(rewards)
 
@@ -77,8 +64,7 @@ class MAA2C:
         if all(dones) or step == max_steps-1:
 
           dones = [1 for _ in range(self.num_agents)]
-#           trajectory.append([states_policy,actions,rewards,states_value])
-          trajectory.append([states_policy,actions,rewards,states_policy])
+          trajectory.append([states,next_states,actions,rewards])
           print("*"*100)
           print("EPISODE: {} | REWARD: {} \n".format(episode,np.round(episode_reward,decimals=4)))
           print("*"*100)
@@ -87,14 +73,13 @@ class MAA2C:
           break
         else:
           dones = [0 for _ in range(self.num_agents)]
-#           trajectory.append([states_policy,actions,rewards,states_value])
-          trajectory.append([states_policy,actions,rewards,states_policy])
-          states_policy = next_states
+          trajectory.append([states,next_states,actions,rewards])
+          states = next_states
       
 #       make a directory called models
-      if episode%500:
-        torch.save(self.agents.value_network.state_dict(), "./models/four_agent/value_net_xavier_init_value_lr_2e-4_policy_lr_2e-4_entropy_pen_0.008.pt")
-        torch.save(self.agents.policy_network.state_dict(), "./models/four_agent/policy_net_xavier_init_value_lr_2e-4_policy_lr_2e-4_entropy_pen_0.008.pt")      
-        
+      if episode%100:
+        torch.save(self.agents.value_network.state_dict(), "./models/3_agents/value_net_lr_2e-4_policy_lr_2e-4_with_grad_norm_0.5_entropy_pen_0.008_xavier_uniform_init_clamp_logs.pt")
+        torch.save(self.agents.policy_network.state_dict(), "./models/3_agents/policy_net_lr_2e-4_value_lr_2e-4_with_grad_norm_0.5_entropy_pen_0.008_xavier_uniform_init_clamp_logs.pt")      
+
       self.update(trajectory,episode) 
 
