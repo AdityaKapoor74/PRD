297ef0899663721d4e2107a0aeebf54a966137f9
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2020-08-10 15:14:04 +0530

    Update main.py

--- PartialRewardDecoupling/main.py
+++ PartialRewardDecoupling/main.py
@@ -4,6 +4,6 @@ from maa2c import MAA2C
 
 if __name__ == '__main__':
 	env = make_env(scenario_name="simple_spread",benchmark=False)
-	ma_controller = MAA2C(env)
+	ma_controller = MAA2C(env,False)
 
 	ma_controller.run(10000000,300)
