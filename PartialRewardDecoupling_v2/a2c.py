8c9ced5deda6414e27c1964100f3e1bb6fd6e8e7
Aditya Kapoor <adityakapoor074@gmail.com>
2020-10-19 13:19:52 +0530

    fixing bugs 2

--- PartialRewardDecoupling_v2/a2c.py
+++ PartialRewardDecoupling_v2/a2c.py
@@ -6,50 +6,53 @@ import torch.nn.functional as F
 # *******************************************
 # Q(s,a) 
 # *******************************************
-class GenericNetwork(nn.Module):
-	def __init__(
+
+def create_model(
 	layer_sizes: Tuple,
 	weight_init: str = "xavier_uniform",
 	activation_func: str = "relu"
 	):
-		super(GenericNetwork,self).__init__()
 
-		layers = []
-		limit = len(layer_sizes)
+	layers = []
+	limit = len(layer_sizes)
 
-		# add more activations
-		activation = nn.Tanh() if activation_func == "tanh" else nn.ReLU()
+	# add more activations
+	activation = nn.Tanh() if activation_func == "tanh" else nn.ReLU()
 
-		# add more weight init
-		if weight_init == "xavier_uniform":
-			weight_init = torch.nn.init.xavier_uniform_
-		elif weight_init == "xavier_normal":
-			weight_init = torch.nn.init.xavier_normal_
+	# add more weight init
+	if weight_init == "xavier_uniform":
+		weight_init = torch.nn.init.xavier_uniform_
+	elif weight_init == "xavier_normal":
+		weight_init = torch.nn.init.xavier_normal_
 
-		for layer in range(limit - 1):
-			act = activation if layer < limit - 2 else nn.Identity()
-			layers += [nn.Linear(layer_sizes[layer], layer_sizes[layer + 1])]
-			weight_init(layers[-1].weight)
-			layers += [act]
+	for layer in range(limit - 1):
+		act = activation if layer < limit - 2 else nn.Identity()
+		layers += [nn.Linear(layer_sizes[layer], layer_sizes[layer + 1])]
+		weight_init(layers[-1].weight)
+		layers += [act]
 
-		return nn.Sequential(*layers)
+	return nn.Sequential(*layers)
 
 
 
 
 class QValueNetwork(nn.Module):
 
-	def __init__(self,curr_agent_sizes, other_agent_sizes, common_sizes):
+	def __init__(self,
+		curr_agent_sizes, 
+		other_agent_sizes, 
+		common_sizes
+		):
 		super(QValueNetwork,self).__init__()
 
-		self.current_agent = GenericNetwork(curr_agent_sizes)
-		self.other_agent = GenericNetwork(other_agent_sizes)
-		self.common = GenericNetwork(common_sizes)
+		self.current_agent = create_model(curr_agent_sizes)
+		self.other_agent = create_model(other_agent_sizes)
+		self.common = create_model(common_sizes)
 
 
 
 
-	def forward(current_agent_states, other_agent_states):
+	def forward(self,current_agent_states, other_agent_states):
 
 		curr_agent_outputs = torch.FloatTensor([[self.current_agent(torch.cat([current_agent[j],self.one_hots[i]])) for i in range(self.action_dim)] for j in range(self.num_agents)])
 		other_agents_states_actions = torch.zeros(self.num_agents,self.num_agents-1,self.action_dim)
@@ -58,10 +61,13 @@ class QValueNetwork(nn.Module):
 
 
 class PolicyNetwork(nn.Module):
-	def __init__(policy_sizes):
+	def __init__(
+		self,
+		policy_sizes
+		):
 		super(PolicyNetwork,self).__init__()
 
-		self.policy = GenericNetwork(policy_sizes)
+		self.policy = create_model(policy_sizes)
 
-	def forward(states):
+	def forward(self,states):
 		return F.softmax(self.policy(states),-1)
