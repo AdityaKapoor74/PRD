910b63a263148bcb20da8f20971771c6b1861582
Aditya Kapoor <adityakapoor074@gmail.com>
2021-01-20 13:01:57 +0530

    update

--- PartialRewardDecoupling_GNN_weight_net_v2/maa2c.py
+++ PartialRewardDecoupling_GNN_weight_net_v2/maa2c.py
@@ -113,6 +113,7 @@ class MAA2C:
 							FP[i] += 1
 						else:
 							TN[i] += 1
+
 		# calculate accuracy/precision/recall before calculating rates
 		for i in range(self.num_agents):
 			TP_rate[i] = TP[i]/(TP[i]+FN[i])
@@ -241,37 +242,6 @@ class MAA2C:
 			   
 		return graph
 
-
-	# def make_gif(self, images, fname, duration=2, true_image=False,salience=False,salIMGS=None):
-	# 	import moviepy.editor as mpy
-
-	# 	def make_frame(t):
-	# 		try:
-	# 			x = images[int(len(images)/duration*t)]
-	# 		except:
-	# 			x = images[-1]
-
-	# 		if true_image:
-	# 			return x.astype(np.uint8)
-	# 		else:
-	# 			return ((x+1)/2*255).astype(np.uint8)
-
-	# 	def make_mask(t):
-	# 		try:
-	# 			x = salIMGS[int(len(salIMGS)/duration*t)]
-	# 		except:
-	# 			x = salIMGS[-1]
-	# 		return x
-
-	# 	clip = mpy.VideoClip(make_frame, duration=duration)
-	# 	if salience == True:
-	# 		mask = mpy.VideoClip(make_mask, ismask=True,duration= duration)
-	# 		clipB = clip.set_mask(mask)
-	# 		clipB = clip.set_opacity(0)
-	# 		mask = mask.set_opacity(0.1)
-	# 		mask.write_gif(fname, fps = len(images) / duration,verbose=False)
-	# 	else:
-	# 		clip.write_gif(fname, fps = len(images) / duration,verbose=False)
 
 	def make_gif(self,images,fname,fps=10, scale=1.0):
 		from moviepy.editor import ImageSequenceClip
