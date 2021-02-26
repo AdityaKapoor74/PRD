f589eede1cdf5f7b700fae53d29f3b494d1d03b1
Aditya Kapoor <adityakapoor074@gmail.com>
2021-02-27 01:35:07 +0530

    GAE trace_decay=1

--- PRD_GAT_1/a2c_agent.py
+++ PRD_GAT_1/a2c_agent.py
@@ -20,7 +20,7 @@ class A2CAgent:
 		entropy_pen=0.008, 
 		gamma=0.99,
 		lambda_ = 1e-3,
-		trace_decay = 0.98,
+		trace_decay = 1.0,
 		gif = False
 		):
 
@@ -101,6 +101,7 @@ class A2CAgent:
 			previous_value = 0
 			running_advants = 0
 			rewards = rewards.unsqueeze(-1)
+			dones = dones.unsqueeze(-1)
 			for t in reversed(range(0, len(rewards))):
 				running_delta = rewards[t] + (self.gamma * previous_value * dones[t]) - values.data[t]
 				previous_value = values.data[t]
