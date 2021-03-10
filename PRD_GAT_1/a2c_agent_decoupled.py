bf34c659a7e996578ff9d67d8bf1d7f37306863f
Aditya Kapoor <adityakapoor074@gmail.com>
2021-03-10 13:14:22 +0530

    correction

--- PRD_GAT_1/a2c_agent_decoupled.py
+++ PRD_GAT_1/a2c_agent_decoupled.py
@@ -20,8 +20,8 @@ class A2CAgent:
 		policy_lr=2e-4, 
 		entropy_pen=0.008, 
 		gamma=0.99,
-		lambda_ = 0.001,
-		lambda__ = 0.001,
+		lambda_ = 0.0001,
+		lambda__ = 0.0001,
 		trace_decay = 0.95,
 		gif = False
 		):
