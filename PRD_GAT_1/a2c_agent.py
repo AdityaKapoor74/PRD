216849a3cf0c0aabce7be840e61605e2a98f4919
Aditya Kapoor <adityakapoor074@gmail.com>
2021-03-11 19:07:08 +0530

    Experiment10_1

--- PRD_GAT_1/a2c_agent.py
+++ PRD_GAT_1/a2c_agent.py
@@ -171,10 +171,6 @@ class A2CAgent:
 	# we need a TxNxN vector so inflate the discounted rewards by N --> cloning the discounted rewards for an agent N times
 		discounted_rewards = self.calculate_returns(rewards,self.gamma).unsqueeze(-2).repeat(1,self.num_agents,1)
 		discounted_rewards = torch.transpose(discounted_rewards,-1,-2)
-		print("WEIGHT ASS")
-		print(self.weight_assignment.unsqueeze(0).repeat(weights.shape[0],1,1))
-		print("WEIGHT")
-		print(weights)
 		value_loss = F.smooth_l1_loss(V_values,discounted_rewards) + self.weight_loss(self.weight_assignment.unsqueeze(0).repeat(weights.shape[0],1,1),weights)#self.lambda_*torch.sum(weights)#self.lambda_*F.smooth_l1_loss(self.weight_assignment.unsqueeze(0).repeat(weights.shape[0],1,1),weights)
 	# # ***********************************************************************************
 	# 	#update actor (policy net)
