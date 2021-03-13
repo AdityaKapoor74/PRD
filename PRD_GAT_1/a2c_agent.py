4836d7f4e374eaec26aa74f1578d7b7dec6dcd51
Aditya Kapoor <adityakapoor074@gmail.com>
2021-03-14 02:42:16 +0530

    Experiment15_4

--- PRD_GAT_1/a2c_agent.py
+++ PRD_GAT_1/a2c_agent.py
@@ -19,7 +19,7 @@ class A2CAgent:
 		policy_lr=2e-4, 
 		entropy_pen=0.008, 
 		gamma=0.99,
-		lambda_ = 1e-5,
+		lambda_ = 1e-3,
 		trace_decay = 0.98,
 		gif = False
 		):
@@ -41,9 +41,10 @@ class A2CAgent:
 
 
 		# self.critic_preprocess_input_dim = 2*3+2 # (pose,vel,goal pose, paired agent goal pose)
-		self.critic_preprocess_input_dim = 2*3+1 #2*3+2
+		self.critic_preprocess_input_dim = 2*3+2 #2*3+2
 		self.critic_output_dim = 1
-		self.critic_network = CriticNetwork(self.critic_preprocess_input_dim, 16, self.critic_preprocess_input_dim, 16, 16+self.env.action_space[0].n, self.critic_output_dim, self.num_agents, self.env.action_space[0].n).to(self.device)
+		# self.critic_network = CriticNetwork(self.critic_preprocess_input_dim, 16, self.critic_preprocess_input_dim, 16, 16+self.env.action_space[0].n, self.critic_output_dim, self.num_agents, self.env.action_space[0].n).to(self.device)
+		self.critic_network = CriticNetwork(self.critic_preprocess_input_dim, 16, 1, 4, 16+self.env.action_space[0].n, self.critic_output_dim, self.num_agents, self.env.action_space[0].n).to(self.device)
 
 		self.policy_input_dim = 2*(3+2*(self.num_agents-1)) #2 for pose, 2 for vel and 2 for goal of current agent and rest (2 each) for relative position and relative velocity of other agents
 		self.policy_output_dim = self.env.action_space[0].n
--- PRD_GAT_1/maa2c.py
+++ PRD_GAT_1/maa2c.py
@@ -41,26 +41,26 @@ class MAA2C:
 
 
 		if not(self.gif) and self.save:
-			critic_dir = '../../models/Experiment15_3/critic_networks/'
+			critic_dir = '../../models/Experiment15_4/critic_networks/'
 			try: 
 				os.makedirs(critic_dir, exist_ok = True) 
 				print("Critic Directory created successfully") 
 			except OSError as error: 
 				print("Critic Directory can not be created") 
-			actor_dir = '../../models/Experiment15_3/actor_networks/'
+			actor_dir = '../../models/Experiment15_4/actor_networks/'
 			try: 
 				os.makedirs(actor_dir, exist_ok = True) 
 				print("Actor Directory created successfully") 
 			except OSError as error: 
 				print("Actor Directory can not be created")  
-			weight_dir = '../../weights/Experiment15_3/'
+			weight_dir = '../../weights/Experiment15_4/'
 			try: 
 				os.makedirs(weight_dir, exist_ok = True) 
 				print("Weights Directory created successfully") 
 			except OSError as error: 
 				print("Weights Directory can not be created") 
 
-			tensorboard_dir = '../../runs/Experiment15_3/'
+			tensorboard_dir = '../../runs/Experiment15_4/'
 
 
 			# paths for models, tensorboard and gifs
@@ -70,7 +70,7 @@ class MAA2C:
 			self.filename = weight_dir+str(self.date_time)+'_VN_GAT1_PREPROC_GAT1_FC1_lr'+str(self.agents.value_lr)+'_PN_FC2_lr'+str(self.agents.policy_lr)+'_GradNorm0.5_Entropy'+str(self.agents.entropy_pen)+'_trace_decay'+str(self.agents.trace_decay)+'_lambda'+str(self.agents.lambda_)+'tanh.txt'
 
 		elif self.gif:
-			gif_dir = '../../gifs/Experiment15_3/'
+			gif_dir = '../../gifs/Experiment15_4/'
 			try: 
 				os.makedirs(gif_dir, exist_ok = True) 
 				print("Gif Directory created successfully") 
@@ -297,8 +297,9 @@ class MAA2C:
 		# graph = dgl.transform.add_self_loop(graph)
 		graph = dgl.graph((self.src_edges_critic,self.dest_edges_critic),idtype=torch.int32, device=self.device)
 
-		# graph.ndata['obs'] = torch.FloatTensor(states_critic).to(self.device)
-		graph.ndata['obs'] = torch.cat([torch.FloatTensor(states_critic[:,:-2]),self.pairings], dim=-1).to(self.device)
+		graph.ndata['obs'] = torch.FloatTensor(states_critic).to(self.device)
+		graph.ndata['pairings'] = torch.FloatTensor(self.pairings).to(self.device)
+		# graph.ndata['obs'] = torch.cat([torch.FloatTensor(states_critic[:,:-2]),self.pairings], dim=-1).to(self.device)
 		pose_goal = []
 		for pose, goal in zip(states_critic[:,:2],states_critic[:,-2:]):
 			pose_goal.append(np.concatenate([pose,goal]))
