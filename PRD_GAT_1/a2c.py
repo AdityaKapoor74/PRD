49a3836301f1696b710221eb81d2b80ede04071d
Aditya Kapoor <adityakapoor074@gmail.com>
2021-03-13 23:10:48 +0530

    Exp15_3

--- PRD_GAT_1/a2c.py
+++ PRD_GAT_1/a2c.py
@@ -156,7 +156,8 @@ class SoftAttentionInput(nn.Module):
 	def reduce_func(self, nodes):
 		# reduce UDF for equation (3) & (4)
 		# equation (3)
-		alpha = torch.sigmoid(torch.exp(nodes.mailbox['score'] / math.sqrt(self.d_k)).clamp(-5, 5))
+		# alpha = torch.sigmoid(torch.exp(nodes.mailbox['score'] / math.sqrt(self.d_k)).clamp(-5, 5))
+		alpha = torch.sigmoid(nodes.mailbox['score'] / math.sqrt(self.d_k))
 		# equation (4)
 		obs_proc = torch.sum(alpha * nodes.mailbox['value'], dim=1)
 		
@@ -326,7 +327,8 @@ class SoftAttentionWeight(nn.Module):
 	def reduce_func(self, nodes):
 		# reduce UDF for equation (3)
 		# equation (3)
-		w = torch.sigmoid(torch.exp(nodes.mailbox['score'] / math.sqrt(self.d_k)).clamp(-5, 5))
+		# w = torch.sigmoid(torch.exp(nodes.mailbox['score'] / math.sqrt(self.d_k)).clamp(-5, 5))
+		w = torch.sigmoid(nodes.mailbox['score'] / math.sqrt(self.d_k))
 		z = w*nodes.mailbox['act'] + (1-w)*nodes.mailbox['pi']
 		z = z.repeat(1,self.num_agents,1)
 		pi = nodes.mailbox['pi'].repeat(1,self.num_agents,1).reshape(-1,self.place_policies.shape[0],self.place_policies.shape[1],self.place_policies.shape[2])*self.place_policies
--- PRD_GAT_1/maa2c.py
+++ PRD_GAT_1/maa2c.py
@@ -41,26 +41,26 @@ class MAA2C:
 
 
 		if not(self.gif) and self.save:
-			critic_dir = '../../models/Experiment15_1/critic_networks/'
+			critic_dir = '../../models/Experiment15_3/critic_networks/'
 			try: 
 				os.makedirs(critic_dir, exist_ok = True) 
 				print("Critic Directory created successfully") 
 			except OSError as error: 
 				print("Critic Directory can not be created") 
-			actor_dir = '../../models/Experiment15_1/actor_networks/'
+			actor_dir = '../../models/Experiment15_3/actor_networks/'
 			try: 
 				os.makedirs(actor_dir, exist_ok = True) 
 				print("Actor Directory created successfully") 
 			except OSError as error: 
 				print("Actor Directory can not be created")  
-			weight_dir = '../../weights/Experiment15_1/'
+			weight_dir = '../../weights/Experiment15_3/'
 			try: 
 				os.makedirs(weight_dir, exist_ok = True) 
 				print("Weights Directory created successfully") 
 			except OSError as error: 
 				print("Weights Directory can not be created") 
 
-			tensorboard_dir = '../../runs/Experiment15_1/'
+			tensorboard_dir = '../../runs/Experiment15_3/'
 
 
 			# paths for models, tensorboard and gifs
@@ -70,7 +70,7 @@ class MAA2C:
 			self.filename = weight_dir+str(self.date_time)+'_VN_GAT1_PREPROC_GAT1_FC1_lr'+str(self.agents.value_lr)+'_PN_FC2_lr'+str(self.agents.policy_lr)+'_GradNorm0.5_Entropy'+str(self.agents.entropy_pen)+'_trace_decay'+str(self.agents.trace_decay)+'_lambda'+str(self.agents.lambda_)+'tanh.txt'
 
 		elif self.gif:
-			gif_dir = '../../gifs/Experiment15_1/'
+			gif_dir = '../../gifs/Experiment15_3/'
 			try: 
 				os.makedirs(gif_dir, exist_ok = True) 
 				print("Gif Directory created successfully") 
