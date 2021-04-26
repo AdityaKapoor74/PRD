0b0c36942f0e4425a05a0b62fe3381176458c95c
Aditya Kapoor <adityakapoor074@gmail.com>
2021-04-26 15:27:33 +0530

    Graph Attention Network

---
+++ PRD_final/Graph_Attention_Network/main_v5.py
@@ -0,0 +1,28 @@
+# from maa2c import MAA2C
+from maa2c_v5 import MAA2C
+
+from multiagent.environment import MultiAgentEnv
+# from multiagent.scenarios.simple_spread import Scenario
+import multiagent.scenarios as scenarios
+import torch 
+import numpy as np
+
+def make_env(scenario_name, benchmark=False):
+	# load scenario from script
+	scenario = scenarios.load(scenario_name + ".py").Scenario()
+	# scenario = Scenario()
+	# create world
+	world = scenario.make_world()
+	# create multiagent environment
+	if benchmark:
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward_paired_agents, scenario.observation, scenario.benchmark_data, scenario.isFinished)
+	else:
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward_paired_agents, scenario.observation, None, scenario.isFinished)
+	return env
+
+
+
+if __name__ == '__main__':
+	env = make_env(scenario_name="paired_by_sharing_goals",benchmark=False)
+	ma_controller = MAA2C(env,gif=False,save=False)
+	ma_controller.run(50000,2)
