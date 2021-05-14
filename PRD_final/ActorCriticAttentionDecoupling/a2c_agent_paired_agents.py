d6085acd131db98fce984dc2fa7f521c141e3bce
Aditya Kapoor <adityakapoor074@gmail.com>
2021-05-14 11:22:49 +0530

    with_prd_top1

--- PRD_final/ActorCriticAttentionDecoupling/a2c_agent_paired_agents.py
+++ PRD_final/ActorCriticAttentionDecoupling/a2c_agent_paired_agents.py
@@ -207,10 +207,10 @@ class A2CAgent:
 		# Above threshold
 		# masking_advantage = torch.transpose((weights>self.select_above_threshold).int(),-1,-2)
 
-		# advantage = torch.sum(self.calculate_advantages(discounted_rewards, V_values, rewards, dones, True, False) * masking_advantage,dim=-2)
+		advantage = torch.sum(self.calculate_advantages(discounted_rewards, V_values, rewards, dones, True, False) * masking_advantage,dim=-2)
 
 		# SCALING ADVANTAGES
-		advantage = torch.sum(self.calculate_advantages(discounted_rewards, V_values, rewards, dones, True, False) * masking_advantage * self.num_agents,dim=-2)
+		# advantage = torch.sum(self.calculate_advantages(discounted_rewards, V_values, rewards, dones, True, False) * masking_advantage * self.num_agents,dim=-2)
 		
 		# SOFT ADVANTAGES
 		# advantage = torch.sum(self.calculate_advantages(discounted_rewards, V_values, rewards, dones, True, False) * weights ,dim=-2)
