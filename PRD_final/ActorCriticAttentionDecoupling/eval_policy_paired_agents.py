3036b7b587aaada724e0c1634913ba1213169a72
Aditya Kapoor <adityakapoor074@gmail.com>
2021-06-04 02:14:07 +0530

    update:

--- PRD_final/ActorCriticAttentionDecoupling/eval_policy_paired_agents.py
+++ PRD_final/ActorCriticAttentionDecoupling/eval_policy_paired_agents.py
@@ -28,7 +28,7 @@ import math
 from os import listdir
 from os.path import isfile, join
 import pickle, simplejson
-from a2c_paired_agents import ScalarDotProductPolicyNetwork
+from a2c_collision_avoidance import ScalarDotProductPolicyNetwork
 
 
 eval_after = [i*1000 for i in range(1,80)]
@@ -42,9 +42,9 @@ def make_env(scenario_name, benchmark=False):
 	world = scenario.make_world()
 	# create multiagent environment
 	if benchmark:
-		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward_paired_agents, scenario.observation, scenario.benchmark_data, scenario.isFinished)
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data, scenario.isFinished)
 	else:
-		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward_paired_agents, scenario.observation, None, scenario.isFinished)
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, None, scenario.isFinished)
 	return env
 
 
@@ -80,7 +80,7 @@ def run(env, max_episodes, max_steps):
 	final_output_dim = num_actions
 	policy_network = ScalarDotProductPolicyNetwork(obs_input_dim, obs_output_dim, final_input_dim, final_output_dim, num_agents, num_actions, softmax_cut_threshold).to(device)
 
-	policy_eval_dir = '../../../policy_eval/paired_by_sharing_goals/ScalarDotProductPolicyNetwork/'
+	policy_eval_dir = '../../../policy_eval/paired_by_sharing_goals/'
 	try: 
 		os.makedirs(policy_eval_dir, exist_ok = True) 
 		print("Policy Eval Directory created successfully") 
@@ -88,22 +88,20 @@ def run(env, max_episodes, max_steps):
 		print("Policy Eval Directory can not be created")
 
 
-	experiment_type = ["without_prd", "with_prd_top1", "with_prd_top5", "with_prd_top8", "with_prd_soft_adv", "without_prd_scaled", "with_prd_top1_scaled", "with_prd_top5_scaled", "with_prd_top8_scaled", "with_prd_soft_adv_scaled"]
+	experiment_type = ["greedy_policy", "without_prd", "with_prd_top1", "with_prd_top3", "with_prd_soft_adv", "without_prd_scaled", "with_prd_top1_scaled", "with_prd_top3_scaled", "with_prd_soft_adv_scaled"]
 
-	runs = ["run1", "run2", "run3", "run4", "run5"]
+	runs = ["v1", "v2", "v3", "v4", "v5"]
 
 	for run in runs:
-
 		for experiment in experiment_type:
 
 			# Loading models
 			# FOR LOCAL SYSTEM
-			# model_dir_policy = "../../../remote_stations/paired_by_sharing_goals/"+run+"/models/Scalar_dot_product/paired_by_sharing_goals/10_Agents/SingleAttentionMechanism/" + experiment + "/actor_networks/"
+			# model_dir_policy = "../../../remote_stations/collision_avoidance/"+run+"/models/Scalar_dot_product/collision_avoidance/6_Agents/SingleAttentionMechanism/" + experiment + "/actor_networks/"
 			# FOR REMOTE SYSTEM
-			model_dir_policy = "../../../all_models/models/"+run+"/models/Scalar_dot_product/paired_by_sharing_goals/10_Agents/SingleAttentionMechanism/" + experiment + "/actor_networks/"
+			model_dir_policy = "../../../all_models/models"+run+"/" + experiment + "/actor_networks/"
 			
-
-			policy_eval_file_path = policy_eval_dir+'paired_by_sharing_goals_10_Agents_' + experiment + '.txt'
+			policy_eval_file_path = policy_eval_dir+'paired_by_sharing_goals_10_Agents' + experiment + '.txt'
 
 			onlyfiles = [f for f in listdir(model_dir_policy) if isfile(join(model_dir_policy, f))]
 
@@ -145,6 +143,7 @@ def run(env, max_episodes, max_steps):
 
 						total_rewards += np.sum(rewards)
 
+
 						if all(dones):
 							final_time_step = step
 							break
@@ -176,9 +175,9 @@ def run(env, max_episodes, max_steps):
 				simplejson.dump(time_steps, fp)
 				fp.write("\n")
 
+			print("SAVED DATA!")
+
 if __name__ == '__main__':
 	env = make_env(scenario_name="paired_by_sharing_goals",benchmark=False)
 
 	run(env, max_episodes=1000, max_steps=100)
-
-
