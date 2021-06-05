b5ca58f8b789f0520484adb389f50b15b097284f
Aditya Kapoor <adityakapoor074@gmail.com>
2021-06-05 23:41:02 +0530

    v2 75 e-4

--- PRD_final/ActorCriticAttentionDecoupling/main_coma.py
+++ PRD_final/ActorCriticAttentionDecoupling/main_coma.py
@@ -31,39 +31,16 @@ def run_file(dictionary):
 if __name__ == '__main__':
 
 	# VERSION 1
-	dictionary = {
-			"version": 1,
-			"critic_dir": '../../../paired_agents_4_Agents_coma_v1/models/critic_networks/',
-			"actor_dir": '../../../paired_agents_4_Agents_coma_v1/models/actor_networks/',
-			"tensorboard_dir":'../../../paired_agents_4_Agents_coma_v1/runs/',
-			"gif_dir": '../../../paired_agents_4_Agents_coma_v1/gifs/',
-			"env": "paired_by_sharing_goals", 
-			"experiment_type":"coma_v1",
-			"value_lr": 1e-2,
-			"policy_lr": 7.5e-5,
-			"entropy_pen": 0.008, 
-			"gamma": 0.99,
-			"trace_decay": 0.98,
-			"select_above_threshold": 0.1,
-			"softmax_cut_threshold": 0.1,
-			"top_k": 0,
-			"gif": False,
-			"save": True,
-			"max_episodes": 100000,
-			"max_time_steps": 100,
-		}
-
-	# VERSION 2
 	# dictionary = {
-	# 		"version": 2,
-	# 		"critic_dir": '../../../paired_agents_4_Agents_coma_v2/models/critic_networks/',
-	# 		"actor_dir": '../../../paired_agents_4_Agents_coma_v2/models/actor_networks/',
-	# 		"tensorboard_dir":'../../../paired_agents_4_Agents_coma_v2/runs/',
-	# 		"gif_dir": '../../../paired_agents_4_Agents_coma_v2/gifs/',
+	# 		"version": 1,
+	# 		"critic_dir": '../../../paired_agents_4_Agents_coma_v1/models/critic_networks/',
+	# 		"actor_dir": '../../../paired_agents_4_Agents_coma_v1/models/actor_networks/',
+	# 		"tensorboard_dir":'../../../paired_agents_4_Agents_coma_v1/runs/',
+	# 		"gif_dir": '../../../paired_agents_4_Agents_coma_v1/gifs/',
 	# 		"env": "paired_by_sharing_goals", 
-	# 		"experiment_type":"coma_v2",
+	# 		"experiment_type":"coma_v1",
 	# 		"value_lr": 1e-2,
-	# 		"policy_lr": 75e-4, 
+	# 		"policy_lr": 7.5e-5,
 	# 		"entropy_pen": 0.008, 
 	# 		"gamma": 0.99,
 	# 		"trace_decay": 0.98,
@@ -75,6 +52,29 @@ if __name__ == '__main__':
 	# 		"max_episodes": 100000,
 	# 		"max_time_steps": 100,
 	# 	}
+
+	# VERSION 2
+	dictionary = {
+			"version": 2,
+			"critic_dir": '../../../paired_agents_4_Agents_coma_v2/models/critic_networks/',
+			"actor_dir": '../../../paired_agents_4_Agents_coma_v2/models/actor_networks/',
+			"tensorboard_dir":'../../../paired_agents_4_Agents_coma_v2/runs/',
+			"gif_dir": '../../../paired_agents_4_Agents_coma_v2/gifs/',
+			"env": "paired_by_sharing_goals", 
+			"experiment_type":"coma_v2",
+			"value_lr": 1e-2,
+			"policy_lr": 7.5e-4, 
+			"entropy_pen": 0.008, 
+			"gamma": 0.99,
+			"trace_decay": 0.98,
+			"select_above_threshold": 0.1,
+			"softmax_cut_threshold": 0.1,
+			"top_k": 0,
+			"gif": False,
+			"save": True,
+			"max_episodes": 100000,
+			"max_time_steps": 100,
+		}
 
 	# VERSION 3
 	# dictionary = {
