bf845fa4687223d03580a057fcb8dafb98f08e79
Aditya Kapoor <adityakapoor074@gmail.com>
2021-06-14 17:04:49 +0530

    update

--- PRD_final/ActorCriticAttentionDecoupling/main.py
+++ PRD_final/ActorCriticAttentionDecoupling/main.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 	parser.add_argument("--critic_update_type", default= "soft", type=str) # soft, hard
 	parser.add_argument("--critic_update_interval", default= 100, type=int)
 	parser.add_argument("--gae", default= True, type=bool)
-	parser.add_argument("--norm_adv", default= False, type=bool)
+	parser.add_argument("--norm_adv", default= True, type=bool)
 	parser.add_argument("--norm_rew", default= False, type=bool)
 	parser.add_argument("--load_models", default= False, type=bool)
 	parser.add_argument("--critic_saved_path", default= "../../../models/0/predator_prey/without_prd/2_Predators_3_Preys/critic_networks/13-06-2021VN_ATN_FCN_lr0.01_PN_ATN_FCN_lr0.001_GradNorm0.5_Entropy0.001_trace_decay0.98topK_0select_above_threshold0.1softmax_cut_threshold0.1_tau0.001_select_above_threshold0.1_tdlambda0.8_l1pen0.0softtd_lambda_episode50000.pt", type=str)
