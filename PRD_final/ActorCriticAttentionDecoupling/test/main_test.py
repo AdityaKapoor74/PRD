a21dc2bc4637779fee61ad684d996e1313e26728
Aditya Kapoor <adityakapoor074@gmail.com>
2021-06-15 14:39:23 +0530

    update

---
+++ PRD_final/ActorCriticAttentionDecoupling/test/main_test.py
@@ -0,0 +1,60 @@
+# from maa2c import MAA2C
+from maa2c_test import MAA2C
+
+from multiagent.environment import MultiAgentEnv
+# from multiagent.scenarios.simple_spread import Scenario
+import multiagent.scenarios as scenarios
+import torch 
+import numpy as np
+
+def make_env(scenario_name, benchmark=False):
+	# load scenario from script
+	scenario = scenarios.load(scenario_name + ".py").Scenario()
+	# scenario = Scenario()
+	# create world
+	world = scenario.make_world()
+	# create multiagent environment
+	if benchmark:
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data, scenario.isFinished)
+	else:
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, None, scenario.isFinished)
+	return env
+
+
+def run_file(dictionary):
+	env = make_env(scenario_name=dictionary["env"],benchmark=False)
+	ma_controller = MAA2C(env,dictionary)
+	ma_controller.run()
+
+extension = "MLP_CRITIC_STATE" # MLP_CRITIC_STATE, MLP_CRITIC_STATE_ACTION, GNN_CRITIC_STATE, GNN_CRITIC_STATE_ACTION
+test_num = "test1"
+if __name__ == '__main__':
+	dictionary = {
+			"critic_dir": '../../../tests/'+test_num+'/'+extension+'/models/critic_networks/',
+			"actor_dir": '../../../tests/'+test_num+'/'+extension+'/models/actor_networks/',
+			"tensorboard_dir":'../../../tests/'+test_num+'/'+extension+'/runs/',
+			"gif_dir": '../../../tests/'+test_num+'/'+extension+'/gifs/',
+			"env": "paired_by_sharing_goals", 
+			"value_lr": 1e-2, #1e-2 for single head
+			"policy_lr": 1e-3, # 2e-4 for single head
+			"entropy_pen": 8e-3, 
+			"gamma": 0.99,
+			"trace_decay": 0.98,
+			"select_above_threshold": 1e-1,
+			"softmax_cut_threshold": 1e-1,
+			"top_k": 0,
+			"gif": False,
+			"save": True,
+			"learn":True,
+			"max_episodes": 50000,
+			"max_time_steps": 100,
+			"experiment_type": "without_prd",
+			"critic_type": extension,
+			"gif_checkpoint":10,
+			"gae": True,
+			"norm_adv": False,
+			"norm_rew": False,
+		}
+	env = make_env(scenario_name=dictionary["env"],benchmark=False)
+	ma_controller = MAA2C(env,dictionary)
+	ma_controller.run()
