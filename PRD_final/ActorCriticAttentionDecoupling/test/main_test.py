db3e4d02efdb6dd6b439b9a9299c501f2f8ca5df
Aditya Kapoor <adityakapoor074@gmail.com>
2021-06-21 20:46:14 +0530

    test9

--- PRD_final/ActorCriticAttentionDecoupling/test/main_test.py
+++ PRD_final/ActorCriticAttentionDecoupling/test/main_test.py
@@ -27,7 +27,7 @@ def run_file(dictionary):
 	ma_controller.run()
 
 extension = "AttentionCriticV1" # MLP_CRITIC_STATE, MLP_CRITIC_STATE_ACTION, GNN_CRITIC_STATE, GNN_CRITIC_STATE_ACTION, ALL, ALL_W_POL, NonResVx, ResVx, AttentionCriticVx
-test_num = "test9"
+test_num = "test10"
 if __name__ == '__main__':
 	dictionary = {
 			"critic_dir": '../../../../tests/'+test_num+'/models/'+extension+'/critic_networks/',
