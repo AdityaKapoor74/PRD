5c0f331476279066bd58e7f5e8fbf6087170d1ff
Aditya Kapoor <adityakapoor074@gmail.com>
2021-07-09 17:21:40 +0530

    shared

--- PRD_final/ActorCriticAttentionDecoupling/test/main_test.py
+++ PRD_final/ActorCriticAttentionDecoupling/test/main_test.py
@@ -27,10 +27,10 @@ def run_file(dictionary):
 	ma_controller.run()
 
 critic_type = "MLPToGNNV6"
-extension = "MLPToGNNV6_multi_circle_try2" # MLP_CRITIC_STATE, MLP_CRITIC_STATE_ACTION, GNN_CRITIC_STATE, GNN_CRITIC_STATE_ACTION, ALL, ALL_W_POL, NonResVx, ResVx, AttentionCriticV1, MLPToGNN
+extension = "MLPToGNNV6_multi_circle_try1" # MLP_CRITIC_STATE, MLP_CRITIC_STATE_ACTION, GNN_CRITIC_STATE, GNN_CRITIC_STATE_ACTION, ALL, ALL_W_POL, NonResVx, ResVx, AttentionCriticV1, MLPToGNN
 test_num = "multi_circle"
 env_name = "multi_circular"
-experiment_type = "with_prd_soft_adv"
+experiment_type = "without_prd"
 if __name__ == '__main__':
 	dictionary = {
 			"critic_dir": '../../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
