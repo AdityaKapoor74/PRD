646aec742a7679536558f4a0743872f54333355c
Aditya Kapoor <adityakapoor074@gmail.com>
2021-07-28 03:14:49 +0530

    0.005

--- Agent/main.py
+++ Agent/main.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 				"critic_loss_type": "MC",
 				"gamma": 0.99, 
 				"trace_decay": 0.98,
-				"select_above_threshold": 0.001,
+				"select_above_threshold": 0.005,
 				"top_k": 0,
 				"gif": False,
 				"save_model": False,
