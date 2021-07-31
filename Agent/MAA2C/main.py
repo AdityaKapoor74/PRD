d4a67f86f28cc11660e0506dad0b9eff1af6e951
Aditya Kapoor <adityakapoor074@gmail.com>
2021-07-31 17:29:59 +0530

    update

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 		extension = "run"+str(i)
 		test_num = "color_social_dilemma_pt2_8_Agents"
 		env_name = "color_social_dilemma_pt2" 
-		experiment_type = "prd_above_threshold" # prd_above_threshold_decay_episodic, greedy, shared, prd_above_threshold_ascend_episodic
+		experiment_type = "prd_top5" # prd_above_threshold_decay_episodic, greedy, shared, prd_above_threshold_ascend_episodic
 
 		dictionary = {
 				"critic_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
@@ -45,7 +45,7 @@ if __name__ == '__main__':
 				"threshold_min": 0.0, #0.0001
 				"threshold_max": 0.001,
 				"steps_to_take": 10000, #1000
-				"top_k": 0,
+				"top_k": 5,
 				"gif": False,
 				"save_model": True,
 				"save_model_checkpoint": 100,
