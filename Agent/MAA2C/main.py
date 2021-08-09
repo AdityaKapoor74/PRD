84625466c5e2e13094e09b15d2efb2be48ed0aea
Aditya Kapoor <adityakapoor074@gmail.com>
2021-08-10 02:27:00 +0530

    prd 0.01 l1 0.05

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
 				"value_lr": 1e-3, 
 				"policy_lr": 5e-4,
 				"entropy_pen": 8e-3, 
-				"l1_pen": 0.03,
+				"l1_pen": 0.05,
 				"critic_entropy_pen": 0.0,
 				"critic_loss_type": "TD_lambda",
 				"gamma": 0.99, 
