6cd941985c2d98dbd7dc8fdd82c3316da5fb12da
Aditya Kapoor <adityakapoor074@gmail.com>
2021-08-10 13:41:59 +0530

    prd 0.01

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
 		extension = "run"+str(i)
 		test_num = "paired_by_switching_rewards_8_agents_policy_eval" #crossing_8_agents_pen_non_colliding_agents_policy_eval
 		env_name = "paired_by_sharing_goals"
-		experiment_type = "greedy" # prd_above_threshold_decay_episodic, greedy, shared
+		experiment_type = "prd_above_threshold" # prd_above_threshold_decay_episodic, greedy, shared
 
 		dictionary = {
 				"critic_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
@@ -95,7 +95,7 @@ if __name__ == '__main__':
 				"gamma": 0.99, 
 				"trace_decay": 0.98,
 				"lambda": 0.8, #0.8
-				"select_above_threshold": 0.0,
+				"select_above_threshold": 0.01,
 				"threshold_min": 0.0, 
 				"threshold_max": 0.0,
 				"steps_to_take": 50000, 
