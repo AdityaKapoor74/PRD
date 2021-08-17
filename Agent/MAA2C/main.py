50be2967c39b8f97152ac8a4b9f60d1419584bb2
Aditya Kapoor <adityakapoor074@gmail.com>
2021-08-17 12:45:21 +0530

    prd 0.01 l1 0.01 to 0.0 15K

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 		extension = "run"+str(i)
 		test_num = "color_social_dilemma_8_Agents_50K_policy_eval"
 		env_name = "color_social_dilemma_pt2" 
-		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_decay, greedy, shared, prd_above_threshold_ascend, prd_above_threshold_l1_pen_decay
+		experiment_type = "prd_above_threshold_l1_pen_decay" # prd_above_threshold_decay, greedy, shared, prd_above_threshold_ascend, prd_above_threshold_l1_pen_decay
 
 		dictionary = {
 				"critic_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
@@ -39,18 +39,18 @@ if __name__ == '__main__':
 				"value_lr": 1e-3, 
 				"policy_lr": 5e-4, #1e-4
 				"entropy_pen": 8e-3, 
-				"l1_pen": 0.0,
+				"l1_pen": 0.01,
 				"critic_entropy_pen": 0.0,
 				"critic_loss_type": "TD_lambda", #MC
 				"gamma": 0.99, 
 				"trace_decay": 0.98,
 				"lambda": 0.8, #0.8
-				"select_above_threshold": 0.0,
+				"select_above_threshold": 0.01,
 				"threshold_min": 0.0, #0.0001
-				"threshold_max": 0.01,
-				"steps_to_take": 15000,
+				"threshold_max": 0.0,
+				"steps_to_take": 0,
 				"l1_pen_min": 0.0,
-				"l1_pen_steps_to_take": 0,
+				"l1_pen_steps_to_take": 15000,
 				"top_k": 0,
 				"gif": False,
 				"save_model": True,
