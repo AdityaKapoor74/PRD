53d6452cb8da9c896b160e5513e9de10cea43e0a
Aditya Kapoor <adityakapoor074@gmail.com>
2021-08-19 23:38:48 +0530

    prd 0.0 to 0.01 15K

--- Agent/MAA2C/a2c_agent.py
+++ Agent/MAA2C/a2c_agent.py
@@ -63,7 +63,7 @@ class A2CAgent:
 		elif self.env_name in ["color_social_dilemma", "color_social_dilemma_pt2"]:
 			obs_dim = 2*2 + 1 + 2*3
 		elif self.env_name == "team_crossing":
-			obs_dim = 2*3 + 1
+			obs_dim = 2*3 + 1 + (2+1) * (self.num_agents-1)
 
 		self.critic_network = GATCritic(obs_dim, 128, obs_dim+self.num_actions, 128, 128, 1, self.num_agents, self.num_actions).to(self.device)
 		
@@ -73,7 +73,7 @@ class A2CAgent:
 		elif self.env_name in ["color_social_dilemma_pt2"]:
 			obs_dim = 2*2 + 1 + 2*3
 		elif self.env_name in ["team_crossing"]:
-			obs_dim = 2*3 + 1
+			obs_dim = 2*3 + 1 + (2+1) * (self.num_agents-1)
 
 		# MLP POLICY
 		self.policy_network = MLPPolicyNetwork(obs_dim, self.num_agents, self.num_actions).to(self.device)
