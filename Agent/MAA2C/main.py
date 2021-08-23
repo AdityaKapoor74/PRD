7dd04252db0b08dbeecbe66d98939350e2756fde
Aditya Kapoor <adityakapoor074@gmail.com>
2021-08-23 23:54:54 +0530

    shared

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
 		extension = "run"+str(i)
 		test_num = "crossing_8_agents_pen_non_colliding_team_members_DUALGAT_policy_eval" #crossing_8_agents_pen_non_colliding_agents_policy_eval
 		env_name = "crossing"
-		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_decay_episodic, greedy, shared
+		experiment_type = "shared" # prd_above_threshold_decay_episodic, greedy, shared
 
 		dictionary = {
 				"critic_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
@@ -89,9 +89,9 @@ if __name__ == '__main__':
 				"test_num":test_num,
 				"extension":extension,
 				"value_lr": 1e-3, 
-				"policy_lr": 5e-4,
-				"entropy_pen": 4e-3, 
-				"entropy_pen_min": 4e-3,
+				"policy_lr": 1e-4,
+				"entropy_pen": 8e-3, 
+				"entropy_pen_min": 8e-3,
 				"l1_pen": 0.0,
 				"critic_entropy_pen": 0.0,
 				"critic_loss_type": "TD_lambda",
