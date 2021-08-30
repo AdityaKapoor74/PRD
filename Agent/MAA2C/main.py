0c1c6363c64e984aa968bdb4f0240716d3614d9d
Aditya Kapoor <adityakapoor074@gmail.com>
2021-08-30 11:08:04 +0530

    update

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
 				"test_num":test_num,
 				"extension":extension,
 				"value_lr": 1e-3, 
-				"policy_lr": 1e-4,
+				"policy_lr": 5e-4,
 				"entropy_pen": 1e-3, 
 				"entropy_pen_min": 1e-3,
 				"l1_pen": 0.0,
