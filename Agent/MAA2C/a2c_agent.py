26eea967bb2306a5307e55a450805df0611bc04e
Aditya Kapoor <adityakapoor074@gmail.com>
2021-09-03 17:12:52 +0530

    cpu

--- Agent/MAA2C/a2c_agent.py
+++ Agent/MAA2C/a2c_agent.py
@@ -50,7 +50,7 @@ class A2CAgent:
 			self.l1_pen_delta = (self.l1_pen - self.l1_pen_min)/self.l1_pen_steps_to_take
 
 		self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-		# self.device = "cpu"
+		self.device = "cpu"
 		
 		self.num_agents = self.env.n
 		self.num_actions = self.env.action_space[0].n
