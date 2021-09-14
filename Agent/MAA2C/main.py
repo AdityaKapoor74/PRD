6e92c33aee2e5e9e22b9a16a2a905dd8bb45ba1f
Aditya Kapoor <adityakapoor074@gmail.com>
2021-09-14 11:13:32 +0530

    threshold=0.2

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
 				"threshold_max": 0.2,
-				"steps_to_take": 15000, 
+				"steps_to_take": 1000, 
 				"l1_pen_min": 0.0,
 				"l1_pen_steps_to_take": 0,
 				"top_k": 0,
