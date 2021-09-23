9abc200b4709e2508d2d40ea2859a38c8122a8f4
Aditya Kapoor <adityakapoor074@gmail.com>
2021-09-23 17:00:14 +0530

    Transformer

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
 	# crossing_greedy/ crossing_fully_coop /  paired_by_sharing_goals/ crossing_partially_coop
 	for i in range(1,2):
 		extension = "run"+str(i)
-		test_num = "TransformersTest" #crossing_8_agents_pen_non_colliding_agents_policy_eval
+		test_num = "TransformersTest" #TransformersTest
 		env_name = "crossing_greedy"
 		experiment_type = "shared" # prd_above_threshold_decay_episodic, greedy, shared
 
@@ -89,8 +89,8 @@ if __name__ == '__main__':
 				"env": env_name, 
 				"policy_type": "MLP", # MLP/ GCN/ GAT
 				"policy_attention_heads": 0,
-				"critic_type": "TransformerCritic", # TransformersONLY/ GATONLY/ GATv2ONLY/ NormalizedATONLY/ else One Critic for training
-				"critic_attention_heads": [2,4,8],
+				"critic_type": "MultiHeadTransformerCritic", # TransformersONLY/ GATONLY/ GATv2ONLY/ NormalizedATONLY/ else One Critic for training
+				"critic_attention_heads": 2,
 				"test_num":test_num,
 				"extension":extension,
 				"value_lr": 1e-3, 
