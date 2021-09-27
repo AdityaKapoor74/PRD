c599366035a0765b7dc68d2543d5519fde57aa1e
Aditya Kapoor <adityakapoor074@gmail.com>
2021-09-27 17:50:40 +0530

    update

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -81,16 +81,16 @@ if __name__ == '__main__':
 		experiment_type = "shared" # prd_above_threshold_decay_episodic, greedy, shared
 
 		dictionary = {
-				"critic_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
-				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
-				"run_dir":'../../../tests/'+test_num+'/runs/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"env": env_name, 
 				"policy_type": "MLP", # MLP/ GCN/ GAT
 				"policy_attention_heads": 0,
 				"critic_type": "MultiHeadSemiHardAttnTransformerCritickthweight8", # TransformersONLY/ GATONLY/ GATv2ONLY/ NormalizedATONLY/ else One Critic for training
-				"critic_attention_heads": 4,
+				"critic_attention_heads": 8,
+				"critic_dir": '../../../tests/'+critic_type+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
+				"actor_dir": '../../../tests/'+critic_type+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
+				"run_dir":'../../../tests/'+critic_type+'/runs/'+env_name+'_'+experiment_type+'_'+extension+'/',
+				"gif_dir": '../../../tests/'+critic_type+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
+				"policy_eval_dir":'../../../tests/'+test_num+"_"+critic_type+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
+				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
 				"value_lr": 1e-3, 
