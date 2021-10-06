089d376cdc1ee39e95873c79d4aa7a9257ff3772
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-06 12:23:45 +0530

    update:

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -105,8 +105,8 @@ if __name__ == '__main__':
 				"lambda": 0.8, #0.8
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.03,
-				"steps_to_take": 1000, 
+				"threshold_max": 0.1,
+				"steps_to_take": 10000, 
 				"l1_pen_min": 0.0,
 				"l1_pen_steps_to_take": 0,
 				"top_k": 0,
