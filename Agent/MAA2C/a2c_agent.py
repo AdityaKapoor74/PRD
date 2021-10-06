242fbcf4a68e90338bf75709b87d5ed32b931aab
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-06 12:08:28 +0530

    update:

--- Agent/MAA2C/a2c_agent.py
+++ Agent/MAA2C/a2c_agent.py
@@ -142,7 +142,7 @@ class A2CAgent:
 			]
 
 		# TransformerStateTransformerStateAction
-		self.critic_network = TransformerCritic(obs_dim, 128, obs_dim+self.num_actions, 128, 128, 1, self.num_agents, self.num_actions).to(self.device)
+		self.critic_network = DualTransformerCritic(obs_dim, 128, obs_dim+self.num_actions, 128, 128, 1, self.num_agents, self.num_actions).to(self.device)
 
 		if self.env_name in ["paired_by_sharing_goals", "crossing_greedy", "crossing_fully_coop"]:
 			obs_dim = 2*3
