6fdade5e96d0af2f7f65a5a07cf5dbdb09c71293
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-11 17:07:02 +0530

    update

--- Agent/Shared_To_Indiv_Reward/main.py
+++ Agent/Shared_To_Indiv_Reward/main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 		extension = "run"+str(i)
 		test_num = "RewardPredictorTests" #TransformersTest
 		env_name = "paired_by_sharing_goals"
-		experiment_type = "shared" # prd_above_threshold_ascend, greedy, shared
+		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_ascend, greedy, shared
 
 		dictionary = {
 				"policy_type": "MLP", # MLP/ GCN/ GAT
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 				"lambda": 0.8, #0.8
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
+				"threshold_max": 0.05,
 				"steps_to_take": 1000, 
 				"l1_pen_min": 0.0,
 				"l1_pen_steps_to_take": 0,
