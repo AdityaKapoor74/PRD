328c94acb511435e978d4babd1d4884404d9096a
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-11 15:31:33 +0530

    update

--- Agent/Shared_To_Indiv_Reward/main.py
+++ Agent/Shared_To_Indiv_Reward/main.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
 				"eval_policy": False,
 				"save_model": False,
 				"save_model_checkpoint": 1000,
-				"save_comet_ml_plot": False,
+				"save_comet_ml_plot": True,
 				"learn":True,
 				"max_episodes": 50000,
 				"max_time_steps": 100,
