3feff4d3ee1456ce1df0b5a5cee0e8e195c5a55f
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-11 18:05:13 +0530

    update:

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
 		extension = "run"+str(i)
 		test_num = "MAPPOvsMAA2C" #TransformersTest
 		env_name = "crossing_team_greedy"
-		experiment_type = "shared" # prd_above_threshold_ascend, greedy, shared
+		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_ascend, greedy, shared
 
 		dictionary = {
 				"policy_type": "MLP", # MLP/ GCN/ GAT
@@ -106,7 +106,7 @@ if __name__ == '__main__':
 				"lambda": 0.8, #0.8
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.03,
+				"threshold_max": 0.05,
 				"steps_to_take": 1000, 
 				"l1_pen_min": 0.0,
 				"l1_pen_steps_to_take": 0,
