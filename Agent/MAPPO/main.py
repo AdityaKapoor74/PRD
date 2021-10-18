43495f36c7a7a94c0b55693af030f6214349b806
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-18 15:23:37 +0530

    NAT

--- Agent/MAPPO/main.py
+++ Agent/MAPPO/main.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 		extension = "MAPPO"+str(i)
 		test_num = "MAPPO_exps"
 		env_name = "crossing_team_greedy" # paired_by_sharing_goals, color_social_dilemma, crossing
-		experiment_type = "prd_soft_adv_scaled"
+		experiment_type = "prd_above_threshold_ascend"
 
 		dictionary = {
 				"policy_type": "MLP", # MLP/ GCN/ GAT
@@ -55,7 +55,7 @@ if __name__ == '__main__':
 				"lambda": 0.8, #0.8
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
+				"threshold_max": 0.1,
 				"steps_to_take": 1000, 
 				"l1_pen_min": 0.0,
 				"l1_pen_steps_to_take": 0,
