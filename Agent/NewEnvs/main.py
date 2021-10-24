e16bf52f67bfbff6575d1e64690baccd74afbb23
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-24 11:39:50 +0530

    Pursuit Env

--- Agent/NewEnvs/main.py
+++ Agent/NewEnvs/main.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
 				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
-				"value_lr": 1e-3, #1e-3
-				"policy_lr": 1e-4, #prd 1e-4
+				"value_lr": 1e-4, #1e-3
+				"policy_lr": 1e-5, #prd 1e-4
 				"entropy_pen": 1e-2, #8e-3
 				"entropy_pen_min": 1e-2,
 				"l1_pen": 0.0,
