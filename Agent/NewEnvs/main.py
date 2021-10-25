562340ea5a9b65edda5ea1967594d9397e560669
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-25 09:34:51 +0530

    Pursuit Env

--- Agent/NewEnvs/main.py
+++ Agent/NewEnvs/main.py
@@ -23,12 +23,13 @@ if __name__ == '__main__':
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_clip": 0.1,
 				"n_epochs": 10,
+				"sample_batch_size": 20,
 				"update_ppo_agent": 1, # update ppo agent after every 4 episodes
 				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
 				"value_lr": 1e-4, #1e-3
-				"policy_lr": 1e-3, #prd 1e-4
+				"policy_lr": 1e-5, #prd 1e-4
 				"entropy_pen": 1e-2, #8e-3
 				"entropy_pen_min": 1e-2,
 				"l1_pen": 0.0,
