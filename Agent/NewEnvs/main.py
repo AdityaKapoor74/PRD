b640ddacacb6b42846bbcf658e1b0474c1b5ee4d
Aditya Kapoor <adityakapoor074@gmail.com>
2021-10-27 13:24:07 +0530

    prd

--- Agent/NewEnvs/main.py
+++ Agent/NewEnvs/main.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 				"lambda": 0.8, #0.8
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.05,
+				"threshold_max": 0.1,
 				"steps_to_take": 1000, 
 				"l1_pen_min": 0.0,
 				"l1_pen_steps_to_take": 0,
@@ -63,7 +63,7 @@ if __name__ == '__main__':
 				"norm_adv": False,
 				"norm_rew": False,
 			}
-		env = pursuit_v3.env()
+		env = pursuit_v3.env(n_evaders=30, n_pursuers=8, n_catch=2)
 		env.reset() # need to reset before accessing number of agents
 		ma_controller = MAPPO(env,dictionary)
 		ma_controller.run()
