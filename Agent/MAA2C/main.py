0050c6152648e54d6ef6b24120f8598031cc35a0
Aditya Kapoor <adityakapoor074@gmail.com>
2021-11-11 11:53:49 +0530

    prd to beat coma

--- Agent/MAA2C/main.py
+++ Agent/MAA2C/main.py
@@ -40,8 +40,8 @@ if __name__ == '__main__':
 				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
-				"value_lr": 5e-4, #1e-3 
-				"policy_lr": 5e-4, #prd 1e-4
+				"value_lr": 1e-3, #1e-3 
+				"policy_lr": 1e-3, #prd 1e-4
 				"shared_actor_critic_lr": 0.0,
 				"entropy_pen": 8e-3, 
 				"entropy_pen_min": 8e-3,
