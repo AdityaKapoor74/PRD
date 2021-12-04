0cbb24aef2c68489f6e7fb492bb071119bbf08d6
Aditya Kapoor <adityakapoor074@gmail.com>
2021-12-04 13:01:32 +0530

    update

--- Agent/MAPPO/main_ppo.py
+++ Agent/MAPPO/main_ppo.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
 				"value_lr": 1e-3, #1e-3
 				"policy_lr": 1e-4, #prd 1e-4
 				"entropy_pen": 8e-3, #8e-3
-				"entropy_pen_min": 8e-3,
+				"entropy_pen_min": 1e-3,
 				"l1_pen": 0.0,
 				"critic_entropy_pen": 0.0,
 				"critic_loss_type": "TD_lambda",
