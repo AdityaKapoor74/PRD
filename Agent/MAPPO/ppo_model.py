b7c7e9432583e16956b8ae19781ee586b59ac2e8
Aditya Kapoor <adityakapoor074@gmail.com>
2021-12-05 10:46:44 +0530

    update

--- Agent/MAPPO/ppo_model.py
+++ Agent/MAPPO/ppo_model.py
@@ -654,8 +654,8 @@ class TransformerCritic_threshold_pred(nn.Module):
 			query_threshold = self.query_threshold[i](state_embed_threshold)
 			key_threshold = self.key_threshold[i](torch.sum(state_embed_threshold, dim=-2)).unsqueeze(-2).repeat(1,self.num_agents,1)
 			score_threshold = torch.matmul(query_threshold,key_threshold.transpose(1,2))/math.sqrt(self.d_k_threshold)
-			# threshold = (torch.tanh(score)+1)/2.0
-			threshold = torch.sigmoid(score_threshold)
+			threshold = (torch.tanh(score)+1)/2.0
+			# threshold = torch.sigmoid(score_threshold)
 			print("Threshold", torch.mean(threshold).item())
 			weight_diff = torch.relu(weight-threshold)+1e-12
 			weight = torch.div(weight_diff,torch.sum(weight_diff,dim=-1).unsqueeze(-1).repeat(1,1,self.num_agents))
