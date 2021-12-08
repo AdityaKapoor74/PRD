cc3686646813b98a1b9a435a97a0829e3a14dab2
Aditya Kapoor <adityakapoor074@gmail.com>
2021-12-08 14:30:35 +0530

    update

--- Agent/MAPPO/main_ppo.py
+++ Agent/MAPPO/main_ppo.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
 				"max_time_steps": 100,
 				"experiment_type": experiment_type,
 				"gae": True,
-				"norm_adv": False,
+				"norm_adv": True,
 				"norm_rew": False,
 			}
 		env = make_env(scenario_name=dictionary["env"],benchmark=False)
