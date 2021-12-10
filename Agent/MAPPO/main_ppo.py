e24f9b05a541f8c35974b0b374f9787bb9615e41
Aditya Kapoor <adityakapoor074@gmail.com>
2021-12-10 19:29:59 +0530

    update

--- Agent/MAPPO/main_ppo.py
+++ Agent/MAPPO/main_ppo.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_run_"+str(i)
 		test_num = "PRD_2_exps"
 		env_name = "crossing_team_greedy" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "greedy"
+		experiment_type = "prd_above_threshold_ascend"
 
 		dictionary = {
 				"iteration": i,
@@ -51,8 +51,8 @@ if __name__ == '__main__':
 				"test_num":test_num,
 				"extension":extension,
 				"pen_threshold": 0.01,
-				"value_lr": 1e-4, #1e-3
-				"policy_lr": 1e-4, #prd 1e-4
+				"value_lr": 5e-4, #1e-3
+				"policy_lr": 5e-4, #prd 1e-4
 				"entropy_pen": 0.0, #8e-3
 				"entropy_pen_min": 0.0,
 				"l1_pen": 0.0,
