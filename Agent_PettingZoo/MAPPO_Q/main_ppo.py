cf0172124c4dd0ac18be3923e8f501523b3d0b16
Aditya Kapoor <adityakapoor074@gmail.com>
2021-12-14 20:41:13 +0530

    update

--- Agent_PettingZoo/MAPPO_Q/main_ppo.py
+++ Agent_PettingZoo/MAPPO_Q/main_ppo.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"policy_clip": 0.1,
+				"policy_clip": 0.05,
 				"n_epochs": 5,
 				"update_ppo_agent": 1, # update ppo agent after every update_ppo_agent episodes
 				"env": env_name, 
