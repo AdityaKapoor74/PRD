9382e82c909adcce16d77a91581fdcef7cabe312
Aditya Kapoor <adityakapoor074@gmail.com>
2021-12-23 14:47:55 +0530

    update

--- Agent_PettingZoo/MAPPO_Q/main_ppo.py
+++ Agent_PettingZoo/MAPPO_Q/main_ppo.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_run_"+str(i)
 		test_num = "PRD_2_exps"
 		env_name = "pursuit_v4" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "greedy"
+		experiment_type = "prd_above_threshold_ascend"
 
 		dictionary = {
 				"iteration": i,
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 				"lambda": 0.8, #0.8
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.05,
+				"threshold_max": 0.1,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"gif": False,
