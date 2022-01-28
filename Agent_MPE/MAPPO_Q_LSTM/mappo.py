21ff0893d8c55043312beafe7fa1cc52c74eb94b
Aditya Kapoor <adityakapoor074@gmail.com>
2022-01-28 20:54:03 +0530

    update

---
+++ Agent_MPE/MAPPO_Q_LSTM/mappo.py
@@ -0,0 +1,262 @@
+import os
+from comet_ml import Experiment
+import numpy as np
+from ppo_agent import PPOAgent
+import torch
+import datetime
+
+
+
+class MAPPO:
+
+	def __init__(self, env, dictionary):
+		if dictionary["device"] == "gpu":
+			self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+		else:
+			self.device = "cpu"
+		self.env = env
+		self.gif = dictionary["gif"]
+		self.save_model = dictionary["save_model"]
+		self.save_model_checkpoint = dictionary["save_model_checkpoint"]
+		self.save_comet_ml_plot = dictionary["save_comet_ml_plot"]
+		self.learn = dictionary["learn"]
+		self.gif_checkpoint = dictionary["gif_checkpoint"]
+		self.eval_policy = dictionary["eval_policy"]
+		self.num_agents = self.env.n
+		self.num_actions = self.env.action_space[0].n
+		self.date_time = f"{datetime.datetime.now():%d-%m-%Y}"
+		self.env_name = dictionary["env"]
+		self.test_num = dictionary["test_num"]
+		self.max_episodes = dictionary["max_episodes"]
+		self.max_time_steps = dictionary["max_time_steps"]
+		self.experiment_type = dictionary["experiment_type"]
+		self.update_ppo_agent = dictionary["update_ppo_agent"]
+
+		self.batch_size = self.num_agents
+		self.lstm_num_layers = dictionary["lstm_num_layers"]
+		self.lstm_hidden_dim = dictionary["lstm_hidden_dim"]
+
+
+		self.comet_ml = None
+		if self.save_comet_ml_plot:
+			self.comet_ml = Experiment("im5zK8gFkz6j07uflhc3hXk8I",project_name=dictionary["test_num"])
+			self.comet_ml.log_parameters(dictionary)
+
+
+		self.agents = PPOAgent(self.env, dictionary, self.comet_ml)
+
+		if self.save_model:
+			critic_dir = dictionary["critic_dir"]
+			try: 
+				os.makedirs(critic_dir, exist_ok = True) 
+				print("Critic Directory created successfully") 
+			except OSError as error: 
+				print("Critic Directory can not be created") 
+			actor_dir = dictionary["actor_dir"]
+			try: 
+				os.makedirs(actor_dir, exist_ok = True) 
+				print("Actor Directory created successfully") 
+			except OSError as error: 
+				print("Actor Directory can not be created")
+
+			
+			self.critic_model_path = critic_dir+"critic"
+			self.actor_model_path = actor_dir+"actor"
+			
+
+		if self.gif:
+			gif_dir = dictionary["gif_dir"]
+			try: 
+				os.makedirs(gif_dir, exist_ok = True) 
+				print("Gif Directory created successfully") 
+			except OSError as error: 
+				print("Gif Directory can not be created")
+			self.gif_path = gif_dir+self.env_name+'.gif'
+
+
+		if self.eval_policy:
+			self.policy_eval_dir = dictionary["policy_eval_dir"]
+			try: 
+				os.makedirs(self.policy_eval_dir, exist_ok = True) 
+				print("Policy Eval Directory created successfully") 
+			except OSError as error: 
+				print("Policy Eval Directory can not be created")
+
+
+	def split_states(self,states):
+		states_critic = []
+		states_actor = []
+		for i in range(self.num_agents):
+			states_critic.append(states[i][0])
+			states_actor.append(states[i][1])
+
+		states_critic = np.asarray(states_critic)
+		states_actor = np.asarray(states_actor)
+
+		return states_critic,states_actor
+
+
+	def make_gif(self,images,fname,fps=10, scale=1.0):
+		from moviepy.editor import ImageSequenceClip
+		"""Creates a gif given a stack of images using moviepy
+		Notes
+		-----
+		works with current Github version of moviepy (not the pip version)
+		https://github.com/Zulko/moviepy/commit/d4c9c37bc88261d8ed8b5d9b7c317d13b2cdf62e
+		Usage
+		-----
+		>>> X = randn(100, 64, 64)
+		>>> gif('test.gif', X)
+		Parameters
+		----------
+		filename : string
+			The filename of the gif to write to
+		array : array_like
+			A numpy array that contains a sequence of images
+		fps : int
+			frames per second (default: 10)
+		scale : float
+			how much to rescale each image by (default: 1.0)
+		"""
+
+		# copy into the color dimension if the images are black and white
+		if images.ndim == 3:
+			images = images[..., np.newaxis] * np.ones(3)
+
+		# make the moviepy clip
+		clip = ImageSequenceClip(list(images), fps=fps).resize(scale)
+		clip.write_gif(fname, fps=fps)
+
+
+
+
+	def run(self):  
+		if self.eval_policy:
+			self.rewards = []
+			self.rewards_mean_per_1000_eps = []
+			self.timesteps = []
+			self.timesteps_mean_per_1000_eps = []
+			self.collision_rates = []
+			self.collison_rate_mean_per_1000_eps = []
+
+		h_policy = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+		cell_policy = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+
+		h_critic = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+		cell_critic = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+
+		for episode in range(1,self.max_episodes+1):
+
+			states = self.env.reset()
+
+			# if the episode should always start with 0 hidden and cell states
+			h_policy = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+			cell_policy = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+
+			h_critic = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+			cell_critic = torch.zeros(self.lstm_num_layers, self.batch_size, self.lstm_hidden_dim).to(self.device)
+
+			images = []
+
+			states_critic, states_actor = self.split_states(states)
+
+			trajectory = []
+			episode_reward = 0
+			episode_collision_rate = 0
+			episode_goal_reached = 0
+			final_timestep = self.max_time_steps
+			for step in range(1, self.max_time_steps+1):
+
+				if self.gif:
+					# At each step, append an image to list
+					if not(episode%self.gif_checkpoint):
+						images.append(np.squeeze(self.env.render(mode='rgb_array')))
+					# Advance a step and render a new image
+					with torch.no_grad():
+						actions, policy, h_policy, cell_policy = self.agents.get_action(states_actor, h_policy, cell_policy)
+				else:
+					actions, policy, h_policy, cell_policy = self.agents.get_action(states_actor, h_policy, cell_policy)
+
+				one_hot_actions = np.zeros((self.num_agents,self.num_actions))
+				for i,act in enumerate(actions):
+					one_hot_actions[i][act] = 1
+
+				h_critic, cell_critic = self.agents.get_values(states_critic, h_critic, cell_critic, policy.detach(), one_hot_actions)
+
+				next_states, rewards, dones, info = self.env.step(actions)
+				next_states_critic, next_states_actor = self.split_states(next_states)
+
+				collision_rate = [value[1] for value in rewards]
+				goal_reached = [value[2] for value in rewards]
+				rewards = [value[0] for value in rewards]
+				episode_collision_rate += np.sum(collision_rate)
+				episode_goal_reached += np.sum(goal_reached)
+
+
+				if step == self.max_time_steps:
+					dones = [True for _ in range(self.num_agents)]
+
+				self.agents.buffer.states_critic.append(states_critic)
+				self.agents.buffer.states_actor.append(states_actor)
+				self.agents.buffer.actions.append(actions)
+				self.agents.buffer.one_hot_actions.append(one_hot_actions)
+				self.agents.buffer.dones.append(dones)
+				self.agents.buffer.rewards.append(rewards)
+
+				episode_reward += np.sum(rewards)
+
+				states_critic, states_actor = next_states_critic, next_states_actor
+				states = next_states
+
+
+				if self.learn:
+					if all(dones):
+
+						print("*"*100)
+						print("EPISODE: {} | REWARD: {} | TIME TAKEN: {} / {} \n".format(episode,np.round(episode_reward,decimals=4),step,self.max_time_steps))
+						print("*"*100)
+
+						final_timestep = step
+
+						if self.save_comet_ml_plot:
+							self.comet_ml.log_metric('Episode_Length', step, episode)
+							self.comet_ml.log_metric('Reward', episode_reward, episode)
+							self.comet_ml.log_metric('Number of Collision', episode_collision_rate, episode)
+							self.comet_ml.log_metric('Num Agents Goal Reached', episode_goal_reached, episode)
+
+						break
+
+			if self.eval_policy:
+				self.rewards.append(episode_reward)
+				self.timesteps.append(final_timestep)
+				self.collision_rates.append(episode_collision_rate)
+
+			if episode > self.save_model_checkpoint and episode%self.save_model_checkpoint:
+				if self.eval_policy:
+					self.rewards_mean_per_1000_eps.append(sum(self.rewards[episode-self.save_model_checkpoint:episode])/self.save_model_checkpoint)
+					self.timesteps_mean_per_1000_eps.append(sum(self.timesteps[episode-self.save_model_checkpoint:episode])/self.save_model_checkpoint)
+					self.collison_rate_mean_per_1000_eps.append(sum(self.collision_rates[episode-self.save_model_checkpoint:episode])/self.save_model_checkpoint)
+
+
+			if not(episode%self.save_model_checkpoint) and episode!=0 and self.save_model:	
+				torch.save(self.agents.critic_network.state_dict(), self.critic_model_path+'_epsiode'+str(episode)+'.pt')
+				torch.save(self.agents.policy_network.state_dict(), self.actor_model_path+'_epsiode'+str(episode)+'.pt')  
+
+			if self.learn and episode%self.update_ppo_agent == 0 and episode != 0:
+				self.agents.update(episode) 
+			elif self.gif and not(episode%self.gif_checkpoint):
+				print("GENERATING GIF")
+				self.make_gif(np.array(images),self.gif_path)
+
+
+		if self.eval_policy:
+			np.save(os.path.join(self.policy_eval_dir,self.test_num+"reward_list"), np.array(self.rewards), allow_pickle=True, fix_imports=True)
+			np.save(os.path.join(self.policy_eval_dir,self.test_num+"mean_rewards_per_1000_eps"), np.array(self.rewards_mean_per_1000_eps), allow_pickle=True, fix_imports=True)
+			np.save(os.path.join(self.policy_eval_dir,self.test_num+"timestep_list"), np.array(self.timesteps), allow_pickle=True, fix_imports=True)
+			np.save(os.path.join(self.policy_eval_dir,self.test_num+"mean_timestep_per_1000_eps"), np.array(self.timesteps_mean_per_1000_eps), allow_pickle=True, fix_imports=True)
+			np.save(os.path.join(self.policy_eval_dir,self.test_num+"collision_rate_list"), np.array(self.collision_rates), allow_pickle=True, fix_imports=True)
+			np.save(os.path.join(self.policy_eval_dir,self.test_num+"mean_collision_rate_per_1000_eps"), np.array(self.collison_rate_mean_per_1000_eps), allow_pickle=True, fix_imports=True)
+
+			if "prd" in self.experiment_type:
+				np.save(os.path.join(self.policy_eval_dir,self.test_num+"mean_error_rate"), np.array(self.agents.error_rate), allow_pickle=True, fix_imports=True)
+				np.save(os.path.join(self.policy_eval_dir,self.test_num+"average_relevant_set"), np.array(self.agents.average_relevant_set), allow_pickle=True, fix_imports=True)
