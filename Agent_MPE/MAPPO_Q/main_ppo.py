c45ed8933988afa12ad048b26b2eb2640c46bd67
Aditya Kapoor <adityakapoor074@gmail.com>
2022-02-24 18:32:36 +0530

    update

--- Agent_MPE/MAPPO_Q/main_ppo.py
+++ Agent_MPE/MAPPO_Q/main_ppo.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_Q_run_"+str(i)
 		test_num = "PRD_2_MPE"
 		env_name = "crossing_team_greedy" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "shared"
+		experiment_type = "prd_above_threshold_ascend"
 
 		dictionary = {
 				"iteration": i,
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 				"policy_clip": 0.05,
 				"value_clip": 0.05,
 				"n_epochs": 5,
-				"update_ppo_agent": 2, # update ppo agent after every update_ppo_agent episodes
+				"update_ppo_agent": 1, # update ppo agent after every update_ppo_agent episodes
 				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
@@ -51,14 +51,14 @@ if __name__ == '__main__':
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
+				"threshold_max": 0.2,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"gif": False,
 				"gif_checkpoint":1,
 				"load_models": False,
-				"model_path_value": "../../../tests/PRD_2_exps/models/crossing_team_greedy_prd_above_threshold_ascend_MAPPO_run_1/critic_networks/04-12-2021VN_ATN_FCN_lr0.001_PN_ATN_FCN_lr0.0001_GradNorm0.5_Entropy0.008_trace_decay0.98topK_0select_above_threshold0.0l1_pen0.0critic_entropy_pen0.0_epsiode10000.pt",
-				"model_path_policy": "../../../tests/PRD_2_exps/models/crossing_team_greedy_prd_above_threshold_ascend_MAPPO_run_1/actor_networks/04-12-2021_PN_ATN_FCN_lr0.0001VN_SAT_FCN_lr0.001_GradNorm0.5_Entropy0.008_trace_decay0.98topK_0select_above_threshold0.0l1_pen0.0critic_entropy_pen0.0_epsiode10000.pt",
+				"model_path_value": "../../../tests/PRD_2_MPE/models/crossing_team_greedy_shared_MAPPO_Q_run_1/critic_networks/critic_epsiode11000.pt",
+				"model_path_policy": "../../../tests/PRD_2_MPE/models/crossing_team_greedy_shared_MAPPO_Q_run_1/actor_networks/actor_epsiode11000.pt",
 				"eval_policy": True,
 				"save_model": True,
 				"save_model_checkpoint": 1000,
