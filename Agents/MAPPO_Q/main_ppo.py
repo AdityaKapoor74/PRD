f2195f995b224156c3dd42045396e7054bc6a58f
Aditya Kapoor <adityakapoor074@gmail.com>
2022-04-05 00:30:59 +0530

    update

--- Agents/MAPPO_Q/main_ppo.py
+++ Agents/MAPPO_Q/main_ppo.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_Q_run_"+str(i)
 		test_num = "PRD_2_MPE"
 		env_name = "pressureplate-linear-4p-v0" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "shared" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
+		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
 
 		dictionary = {
 				"iteration": i,
@@ -22,8 +22,8 @@ if __name__ == '__main__':
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"policy_clip": 0.2,
-				"value_clip": 0.2,
+				"policy_clip": 0.05,
+				"value_clip": 0.05,
 				"n_epochs": 5,
 				"update_ppo_agent": 1, # update ppo agent after every update_ppo_agent episodes
 				"env": env_name, 
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
+				"threshold_max": 0.05,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"gif": False,
