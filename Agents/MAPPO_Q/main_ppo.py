019ca3614b9b65a9927b0c29f9f83da90420c1b7
Aditya Kapoor <adityakapoor074@gmail.com>
2022-04-04 16:43:14 +0530

    update

--- Agents/MAPPO_Q/main_ppo.py
+++ Agents/MAPPO_Q/main_ppo.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_Q_run_"+str(i)
 		test_num = "PRD_2_MPE"
 		env_name = "pressureplate-linear-6p-v0" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "prd_above_threshold_descend"
+		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_decay
 
 		dictionary = {
 				"iteration": i,
@@ -30,15 +30,15 @@ if __name__ == '__main__':
 				"test_num":test_num,
 				"extension":extension,
 				"value_lr": 1e-3, #1e-3
-				"policy_lr": 1e-3, #prd 1e-4
-				"entropy_pen": 1, #8e-3
+				"policy_lr": 5e-3, #prd 1e-4
+				"entropy_pen": 0.5, #8e-3
 				"critic_weight_entropy_pen": 0.0,
 				"gamma": 0.99, 
 				"gae_lambda": 0.95,
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
-				"select_above_threshold": 0.1,
+				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
+				"threshold_max": 0.05,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"gif": False,
