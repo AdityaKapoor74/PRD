8bafc8b919d89493d83b03876e6afd2774caf7e5
Aditya Kapoor <adityakapoor074@gmail.com>
2022-04-08 16:47:33 +0530

    update

--- Agents/MAPPO_Q/main_ppo.py
+++ Agents/MAPPO_Q/main_ppo.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_Q_run_"+str(i)
 		test_num = "PRD_2_MPE"
 		env_name = "pressureplate-linear-4p-v0" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
+		experiment_type = "shared" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
 
 		dictionary = {
 				"iteration": i,
@@ -31,14 +31,14 @@ if __name__ == '__main__':
 				"extension":extension,
 				"value_lr": 7e-4, #1e-3
 				"policy_lr": 7e-4, #prd 1e-4
-				"entropy_pen": 0.4, #8e-3
+				"entropy_pen": 0.5, #8e-3
 				"critic_weight_entropy_pen": 0.0,
 				"gamma": 0.99, 
 				"gae_lambda": 0.95,
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.05,
+				"threshold_max": 0.0,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"gif": False,
@@ -46,12 +46,12 @@ if __name__ == '__main__':
 				"load_models": False,
 				"model_path_value": "../../../tests/PRD_2_MPE/models/pressureplate-linear-6p-v0_shared_MAPPO_Q_run_1/critic_networks/critic_epsiode1000.pt",
 				"model_path_policy": "../../../tests/PRD_2_MPE/models/pressureplate-linear-6p-v0_shared_MAPPO_Q_run_1/actor_networks/actor_epsiode1000.pt",
-				"eval_policy": False,
+				"eval_policy": True,
 				"save_model": True,
 				"save_model_checkpoint": 1000,
-				"save_comet_ml_plot": True,
+				"save_comet_ml_plot": False,
 				"learn":True,
-				"max_episodes": 30000,
+				"max_episodes": 20000,
 				"max_time_steps": 70,
 				"experiment_type": experiment_type,
 				"norm_adv": False,
