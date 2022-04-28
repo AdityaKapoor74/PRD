42efd0ad14adc698899335df9b8f98bf65d4a539
Aditya Kapoor <adityakapoor074@gmail.com>
2022-04-28 20:29:08 +0530

    update

--- Agents/MAPPO_Q/main_ppo_traffic_junc.py
+++ Agents/MAPPO_Q/main_ppo_traffic_junc.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_Q_run_"+str(i)
 		test_num = "PRD_2_MPE"
 		env_name = "ma_gym:TrafficJunction10-v0"
-		experiment_type = "prd_above_threshold" # shared, prd_above_threshold_ascend, prd_above_threshold_decay, prd_above_threshold
+		experiment_type = "shared" # shared, prd_above_threshold_ascend, prd_above_threshold_decay, prd_above_threshold
 
 		dictionary = {
 				"iteration": i,
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 				"extension":extension,
 				"value_lr": 7e-4, #1e-3
 				"policy_lr": 7e-4, #prd 1e-4
-				"entropy_pen": 1e-3, #8e-3
+				"entropy_pen": 8e-3, #8e-3
 				"critic_weight_entropy_pen": 0.0,
 				"gamma": 0.99, 
 				"gae_lambda": 0.95,
@@ -51,7 +51,7 @@ if __name__ == '__main__':
 				"save_model_checkpoint": 1000,
 				"save_comet_ml_plot": True,
 				"learn":True,
-				"max_episodes": 10000,
+				"max_episodes": 20000,
 				"max_time_steps": 40,
 				"experiment_type": experiment_type,
 				"norm_adv": False,
