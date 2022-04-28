ff1abd0001f21b68d1e1ce9e057a61c776d238b4
Aditya Kapoor <adityakapoor074@gmail.com>
2022-04-28 11:40:43 +0530

    update

--- Agents/MAPPO_Q_Semi_Hard_Attn/main_ppo.py
+++ Agents/MAPPO_Q_Semi_Hard_Attn/main_ppo.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_Q_Semi_Hard_Attn_run_"+str(i)
 		test_num = "PRD_2_MPE"
 		env_name = "pressureplate-linear-4p-v0" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "prd_above_threshold" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
+		experiment_type = "shared" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
 
 		dictionary = {
 				"iteration": i,
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 				"extension":extension,
 				"value_lr": 7e-4, #1e-3
 				"policy_lr": 7e-4, #prd 1e-4
-				"entropy_pen": 0.4, #8e-3
+				"entropy_pen": 1e-1, #8e-3
 				"critic_weight_entropy_pen": 0.0,
 				"gamma": 0.99, 
 				"gae_lambda": 0.95,
@@ -49,7 +49,7 @@ if __name__ == '__main__':
 				"eval_policy": True,
 				"save_model": True,
 				"save_model_checkpoint": 1000,
-				"save_comet_ml_plot": False,
+				"save_comet_ml_plot": True,
 				"learn":True,
 				"max_episodes": 20000,
 				"max_time_steps": 70,
