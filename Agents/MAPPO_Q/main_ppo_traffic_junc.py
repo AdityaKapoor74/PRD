a65ae77d6abef40d7ef045c48aa856615cb9b7ec
Aditya Kapoor <adityakapoor074@gmail.com>
2022-05-13 20:37:01 +0530

    update

--- Agents/MAPPO_Q/main_ppo_traffic_junc.py
+++ Agents/MAPPO_Q/main_ppo_traffic_junc.py
@@ -22,16 +22,16 @@ if __name__ == '__main__':
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"policy_clip": 0.05,
-				"value_clip": 0.05,
+				"policy_clip": 0.01,
+				"value_clip": 0.01,
 				"n_epochs": 5,
 				"update_ppo_agent": 2, # update ppo agent after every update_ppo_agent episodes
 				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
-				"value_lr": 1e-3, #1e-3
-				"policy_lr": 1e-3, #prd 1e-4
-				"entropy_pen": 7e-4, #8e-3
+				"value_lr": 5e-4, #1e-3
+				"policy_lr": 5e-4, #prd 1e-4
+				"entropy_pen": 0.0, #8e-3
 				"critic_weight_entropy_pen": 0.0,
 				"gamma": 0.99, 
 				"gae_lambda": 0.95,
