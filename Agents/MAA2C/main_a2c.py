a6f80e86257110af70288c49b704d4ef9258b934
Aditya Kapoor <adityakapoor074@gmail.com>
2022-07-28 02:10:36 +0530

    update

--- Agents/MAA2C/main_a2c.py
+++ Agents/MAA2C/main_a2c.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
 				"extension":extension,
 				"iteration": i,
 				"device": "gpu",
-				"value_lr": 5e-4, #1e-3 
-				"policy_lr": 5e-4, #prd 1e-4
+				"value_lr": 1e-3, #1e-3 
+				"policy_lr": 7e-4, #prd 1e-4
 				"grad_clip_critic": 0.5,
 				"grad_clip_actor": 0.5,
 				"entropy_pen": 4e-1, #8e-3
