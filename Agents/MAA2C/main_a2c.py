26805d41a55533493e998d7f35130474a4e4a551
Aditya Kapoor <adityakapoor074@gmail.com>
2022-07-27 20:00:29 +0530

    update

--- Agents/MAA2C/main_a2c.py
+++ Agents/MAA2C/main_a2c.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
 				"policy_lr": 7e-4, #prd 1e-4
 				"grad_clip_critic": 10.0,
 				"grad_clip_actor": 10.0,
-				"entropy_pen": 0.0, #8e-3
-				"entropy_pen_min": 0.0, #8e-3
+				"entropy_pen": 0.5, #8e-3
+				"entropy_pen_min": 0.5, #8e-3
 				"critic_entropy_pen": 0.0,
 				"critic_loss_type": "TD_lambda",
 				"gamma": 0.99, 
