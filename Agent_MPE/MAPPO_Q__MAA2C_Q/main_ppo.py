0b30ca171262eed2bfc33fecf9740b166479727f
Aditya Kapoor <adityakapoor074@gmail.com>
2022-08-01 18:26:05 +0530

    update files

--- Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
+++ Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
@@ -23,14 +23,14 @@ def run_file(dictionary):
 if __name__ == '__main__':
 
 	for i in range(1,6):
-		extension = "MAPPO_Q_run_"+str(i)
+		extension = "MAA2C_Q_run_"+str(i)
 		test_num = "MPE"
 		env_name = "crossing_greedy"
 		experiment_type = "prd_above_threshold" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
 
 		dictionary = {
 				"iteration": i,
-				"update_type": "ppo",
+				"update_type": "a2c",
 				"grad_clip_critic": 10.0,
 				"grad_clip_actor": 10.0,
 				"device": "gpu",
