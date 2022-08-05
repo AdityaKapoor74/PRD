f6b48b16b083a22c9e31b24450890dcbad9465e0
Aditya Kapoor <adityakapoor074@gmail.com>
2022-08-05 22:17:32 +0530

    update

--- Agents/MAA2C/main_a2c.py
+++ Agents/MAA2C/main_a2c.py
@@ -20,16 +20,16 @@ if __name__ == '__main__':
 				"iteration": i,
 				"device": "gpu",
 				"value_lr": 5e-4, #1e-3 
-				"policy_lr": 3e-4, #prd 1e-4
+				"policy_lr": 5e-4, #prd 1e-4
 				"grad_clip_critic": 0.5,
 				"grad_clip_actor": 0.5,
-				"entropy_pen": 5e-1, #8e-3
-				"entropy_pen_min": 5e-1, #8e-3
+				"entropy_pen": 4e-1, #8e-3
+				"entropy_pen_min": 4e-1, #8e-3
 				"critic_entropy_pen": 0.0,
 				"critic_loss_type": "TD_lambda",
 				"gamma": 0.99, 
-				"trace_decay": 0.98,
-				"lambda": 0.8, #0.8
+				"trace_decay": 0.95,
+				"lambda": 0.95, #0.8
 				"select_above_threshold": 0.1,
 				"threshold_min": 0.0, 
 				"threshold_max": 0.1,
