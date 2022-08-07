d704861feba5f949f799b3a5256a427277f0eba9
Aditya Kapoor <adityakapoor074@gmail.com>
2022-08-07 17:46:54 +0530

    update

--- Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
+++ Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
@@ -36,15 +36,15 @@ PRD_MAPPO_Q: value_lr = 5e-4; policy_lr = 3e-4; entropy_pen = 8e-3; grad_clip_cr
 
 if __name__ == '__main__':
 
-	for i in range(1,3):
-		extension = "MAAC_Q_run_"+str(i)
+	for i in range(1,2):
+		extension = "MAPPO_Q_run_"+str(i)
 		test_num = "MPE"
 		env_name = "crossing_team_greedy"
-		experiment_type = "shared" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
 
 		dictionary = {
 				"iteration": i,
-				"update_type": "a2c",
+				"update_type": "ppo",
 				"grad_clip_critic": 0.5,
 				"grad_clip_actor": 0.5,
 				"device": "gpu",
@@ -67,9 +67,9 @@ if __name__ == '__main__':
 				"gamma": 0.99, 
 				"gae_lambda": 0.95,
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
-				"select_above_threshold": 0.1,
+				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
+				"threshold_max": 0.1,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"gif": False,
