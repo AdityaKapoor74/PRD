d2184846fc73267af395240a79d8f6b488b57a84
Aditya Kapoor <adityakapoor074@gmail.com>
2022-08-08 00:50:38 +0530

    update

--- Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
+++ Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
@@ -37,10 +37,10 @@ PRD_MAPPO_Q: value_lr = 5e-4; policy_lr = 3e-4; entropy_pen = 8e-3; grad_clip_cr
 if __name__ == '__main__':
 
 	for i in range(1,3):
-		extension = "MAA2C_Q_run_"+str(i)
+		extension = "MAAC_Q_run_"+str(i)
 		test_num = "MPE"
 		env_name = "paired_by_sharing_goals"
-		experiment_type = "prd_above_threshold" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
+		experiment_type = "shared" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
 
 		dictionary = {
 				"iteration": i,
