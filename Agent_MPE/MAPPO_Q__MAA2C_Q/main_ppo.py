5d5b666fe13aff2de6112fc47c2144efe2dc59b3
Aditya Kapoor <adityakapoor074@gmail.com>
2022-08-08 14:11:27 +0530

    update

--- Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
+++ Agent_MPE/MAPPO_Q__MAA2C_Q/main_ppo.py
@@ -53,15 +53,15 @@ if __name__ == '__main__':
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"policy_clip": 0.2,
-				"value_clip": 0.2,
+				"policy_clip": 0.1,
+				"value_clip": 0.1,
 				"n_epochs": 1,
 				"update_ppo_agent": 2, # update ppo agent after every update_ppo_agent episodes
 				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
-				"value_lr": 5e-4, #1e-3
-				"policy_lr": 5e-4, #prd 1e-4
+				"value_lr": 7e-4, #1e-3
+				"policy_lr": 7e-4, #prd 1e-4
 				"entropy_pen": 1e-3, #8e-3
 				"critic_weight_entropy_pen": 0.0,
 				"gamma": 0.99, 
