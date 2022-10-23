9ae624ff2a9cb507f142784bcd362794e6b4360e
Aditya Kapoor <adityakapoor074@gmail.com>
2022-10-23 05:44:43 +0530

    update network

--- Agent_MPE/MAPPO_Q_Semi_Hard_Attn/main_ppo.py
+++ Agent_MPE/MAPPO_Q_Semi_Hard_Attn/main_ppo.py
@@ -29,32 +29,32 @@ PRD_MAPPO_Q: value_lr = 5e-4; policy_lr = 5e-4; entropy_pen = 0.0; grad_clip_cri
 
 if __name__ == '__main__':
 
-	for i in range(1,3):
+	for i in range(1,5):
 		extension = "MAPPO_Q_Semi_Hard_Attn_run_"+str(i)
 		test_num = "MPE"
-		env_name = "paired_by_sharing_goals"
-		experiment_type = "prd_above_threshold" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
+		env_name = "crossing_team_greedy"
+		experiment_type = "shared" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
 
 		dictionary = {
 				"iteration": i,
-				"grad_clip_critic": 0.5,
-				"grad_clip_actor": 0.5,
+				"grad_clip_critic": 10.0,
+				"grad_clip_actor": 10.0,
 				"device": "gpu",
 				"update_learning_rate_with_prd": False,
 				"critic_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/critic_networks/',
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"policy_clip": 0.1,
-				"value_clip": 0.1,
-				"n_epochs": 1,
-				"update_ppo_agent": 2, # update ppo agent after every update_ppo_agent episodes
+				"policy_clip": 0.05,
+				"value_clip": 0.05,
+				"n_epochs": 5,
+				"update_ppo_agent": 5, # update ppo agent after every update_ppo_agent episodes
 				"env": env_name, 
 				"test_num":test_num,
 				"extension":extension,
-				"value_lr": 5e-4, #1e-3
-				"policy_lr": 1e-4, #prd 1e-4
-				"entropy_pen": 8e-3, #8e-3
+				"value_lr": 7e-4, #1e-3
+				"policy_lr": 7e-4, #prd 1e-4
+				"entropy_pen": 0.0, #8e-3
 				"critic_weight_entropy_pen": 0.0,
 				"gamma": 0.99, 
 				"gae_lambda": 0.95,
@@ -69,13 +69,13 @@ if __name__ == '__main__':
 				"load_models": False,
 				"model_path_value": "../../../tests/PRD_2_MPE/models/crossing_team_greedy_prd_above_threshold_MAPPO_Q_run_2/critic_networks/critic_epsiode100000.pt",
 				"model_path_policy": "../../../tests/PRD_2_MPE/models/crossing_team_greedy_prd_above_threshold_MAPPO_Q_run_2/actor_networks/actor_epsiode100000.pt",
-				"eval_policy": True,
-				"save_model": True,
+				"eval_policy": False,
+				"save_model": False,
 				"save_model_checkpoint": 1000,
-				"save_comet_ml_plot": True,
+				"save_comet_ml_plot": False,
 				"learn":True,
 				"max_episodes": 20000,
-				"max_time_steps": 50,
+				"max_time_steps": 100,
 				"experiment_type": experiment_type,
 				"norm_adv": False,
 				"norm_returns": False,
