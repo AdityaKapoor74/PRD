d097dbb580634ea4852b719d1efa577336b7c642
Aditya Kapoor <adityakapoor074@gmail.com>
2022-10-24 19:18:50 +0530

    update

--- Agents/MAPPO_Q__MAA2C_Q/main_ppo.py
+++ Agents/MAPPO_Q__MAA2C_Q/main_ppo.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_Q_run_"+str(i)
 		test_num = "Pressure Plate"
 		env_name = "pressureplate-linear-6p-v0" # paired_by_sharing_goals, color_social_dilemma, crossing_team_greedy, crossing_greedy, crossing_partially_coop, crossing_fully_coop
-		experiment_type = "shared" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
+		experiment_type = "prd_above_threshold" # prd_above_threshold_decay, prd_above_threshold_ascend, shared
 
 		dictionary = {
 				"iteration": i,
