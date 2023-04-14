6d75d10a8e8ed41c20d4991241b76c9cb03d053f
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-14 13:02:19 +0530

    Update train_agent.py

--- Agent_MPE/MAPPO_Q_V/train_agent.py
+++ Agent_MPE/MAPPO_Q_V/train_agent.py
@@ -300,7 +300,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "TEAM COLLISION AVOIDANCE"
 		env_name = "crossing_team_greedy"
-		experiment_type = "shared" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_above_threshold_ascend
 
 		dictionary = {
 				# TRAINING
@@ -357,7 +357,7 @@ if __name__ == '__main__':
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
+				"threshold_max": 0.04,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": False,
