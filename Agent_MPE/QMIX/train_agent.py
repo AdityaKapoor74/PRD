21d4bc7043ec75a1f26bda7711b967de9e76ac53
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-16 14:04:06 +0530

    Update train_agent.py

--- Agent_MPE/QMIX/train_agent.py
+++ Agent_MPE/QMIX/train_agent.py
@@ -299,13 +299,13 @@ if __name__ == '__main__':
 				"epsilon_greedy": 1.0,
 				"epsilon_greedy_min": 0.05,
 				"epsilon_greedy_decay_episodes": 5000,
-				"lambda": 0.95,
+				"lambda": 0.4,
 
 				# ENVIRONMENT
 				"env": env_name,
 
 				# MODEL
-				"learning_rate": 1e-5, #1e-3
+				"learning_rate": 1e-4, #1e-3
 				"grad_clip": 10.0,
 				"rnn_hidden_dim": 64,
 				"hidden_dim": 32,
