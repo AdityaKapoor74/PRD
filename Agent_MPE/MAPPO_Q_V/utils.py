f8f24b3c2eec4defa2e91306d13f40cad3cb9b90
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-22 18:50:03 +0530

    Update utils.py

--- Agent_MPE/MAPPO_Q_V/utils.py
+++ Agent_MPE/MAPPO_Q_V/utils.py
@@ -1,6 +1,6 @@
 import torch
-
 from torch import Tensor
+import numpy as np
 
 
 def gumbel_sigmoid(logits: Tensor, tau: float = 1, hard: bool = False, threshold: float = 0.5) -> Tensor:
@@ -73,6 +73,58 @@ class RolloutBuffer:
 		del self.logprobs[:]
 		del self.rewards[:]
 		del self.dones[:]
+
+
+class RolloutBuffer:
+	def __init__(self, num_episodes, max_time_steps, num_agents, obs_shape_critic, obs_shape_actor, num_actions):
+		self.num_episodes = num_episodes
+		self.max_time_steps = max_time_steps
+		self.num_agents = num_agents
+		self.obs_shape_critic = obs_shape_critic
+		self.obs_shape_actor = obs_shape_actor
+		self.num_actions = num_actions
+		self.episode_num = 0
+		self.time_step = 0
+
+		self.states_critic = np.zeros((num_episodes, max_time_steps, num_agents, obs_shape_critic))
+		self.states_actor = np.zeros((num_episodes, max_time_steps, num_agents, obs_shape_actor))
+		self.logprobs = np.zeros((num_episodes, max_time_steps, num_agents))
+		self.actions = np.zeros((num_episodes, max_time_steps, num_agents), dtype=int)
+		self.one_hot_actions = np.zeros((num_episodes, max_time_steps, num_agents, num_actions))
+		self.rewards = np.zeros((num_episodes, max_time_steps, num_agents))
+		self.dones = np.zeros((num_episodes, max_time_steps, num_agents))
+		self.masks = np.zeros((num_episodes, max_time_steps))
+	
+
+	def clear(self):
+		self.states_critic = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.obs_shape_critic))
+		self.states_actor = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.obs_shape_actor))
+		self.logprobs = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents))
+		self.actions = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents), dtype=int)
+		self.one_hot_actions = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.num_actions))
+		self.rewards = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents))
+		self.dones = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents))
+		self.masks = np.zeros((self.num_episodes, self.max_time_steps))
+
+		self.time_step = 0
+		self.episode_num = 0
+
+	def push(self, state_critic, state_actor, logprobs, actions, one_hot_actions, rewards, dones):
+
+		self.states_critic[self.episode_num][self.time_step] = state_critic
+		self.states_actor[self.episode_num][self.time_step] = state_actor
+		self.logprobs[self.episode_num][self.time_step] = logprobs
+		self.actions[self.episode_num][self.time_step] = actions
+		self.one_hot_actions[self.episode_num][self.time_step] = one_hot_actions
+		self.rewards[self.episode_num][self.time_step] = rewards
+		self.dones[self.episode_num][self.time_step] = dones
+		self.masks[self.episode_num][self.time_step] = 1.
+
+		if self.time_step < self.max_time_steps-1:
+			self.time_step += 1
+		else:
+			self.episode_num += 1
+			self.time_step = 0
 
 
 if __name__ == '__main__':
