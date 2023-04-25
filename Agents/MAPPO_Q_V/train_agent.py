d1af40245cf0189f591811ed3e148643523e0a4a
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-26 01:20:56 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -288,7 +288,7 @@ if __name__ == '__main__':
 				# ACTOR
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.05,
-				"policy_lr": 1e-4, #prd 1e-4
+				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
 				"entropy_pen": 8e-3, #8e-3
 				"gae_lambda": 0.95,
