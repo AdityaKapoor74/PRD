e0bf2ac557c7915db7d28e7a126540d96d06818f
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-26 17:01:41 +0530

    Update main_a2c.py

--- Agents/MAA2C/main_a2c.py
+++ Agents/MAA2C/main_a2c.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 				"extension":extension,
 				"iteration": i,
 				"device": "gpu",
-				"value_lr": 1e-4, #1e-3 
+				"value_lr": 1e-3, #1e-3 
 				"policy_lr": 1e-4, #prd 1e-4
 				"grad_clip_critic": 0.5,
 				"grad_clip_actor": 0.5,
