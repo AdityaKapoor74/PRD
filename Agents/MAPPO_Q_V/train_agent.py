ca642388451465e643bc89e950091b9eae55eaaf
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-26 11:54:37 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -260,7 +260,7 @@ if __name__ == '__main__':
 				"save_model_checkpoint": 1000,
 				"save_comet_ml_plot": True,
 				"learn":True,
-				"max_episodes": 50000,
+				"max_episodes": 80000,
 				"max_time_steps": 70,
 				"experiment_type": experiment_type,
 				"parallel_training": False,
