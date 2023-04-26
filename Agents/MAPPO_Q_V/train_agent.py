019b8974553f251bdc7715eb6108de8211817828
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-26 11:55:53 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -271,13 +271,13 @@ if __name__ == '__main__':
 				"env": env_name,
 
 				# CRITIC
-				"q_value_lr": 5e-4, #1e-3
-				"value_lr": 5e-4, #1e-3
+				"q_value_lr": 3e-4, #1e-3
+				"value_lr": 3e-4, #1e-3
 				"q_weight_decay": 5e-4,
 				"v_weight_decay": 5e-4,
 				"grad_clip_critic": 0.5,
 				"value_clip": 0.05,
-				"enable_hard_attention": True,
+				"enable_hard_attention": False,
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
 				"critic_score_regularizer": 0.0,
@@ -288,7 +288,7 @@ if __name__ == '__main__':
 				# ACTOR
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.05,
-				"policy_lr": 5e-4, #prd 1e-4
+				"policy_lr": 3e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
 				"entropy_pen": 8e-3, #8e-3
 				"gae_lambda": 0.95,
