f96b38d085b29512435b3c9c57fb95592bbe6741
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-26 17:06:33 +0530

    Update train_agent.py

--- Agent_Pursuit/MAPPO_Q_V/train_agent.py
+++ Agent_Pursuit/MAPPO_Q_V/train_agent.py
@@ -277,7 +277,7 @@ if __name__ == '__main__':
 				"v_weight_decay": 5e-4,
 				"grad_clip_critic": 0.5,
 				"value_clip": 0.05,
-				"enable_hard_attention": True,
+				"enable_hard_attention": False,
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
 				"critic_score_regularizer": 0.0,
@@ -290,7 +290,7 @@ if __name__ == '__main__':
 				"policy_clip": 0.05,
 				"policy_lr": 1e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
-				"entropy_pen": 4e-2, #8e-3
+				"entropy_pen": 0.0, #8e-3
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
@@ -299,7 +299,7 @@ if __name__ == '__main__':
 				"top_k": 0,
 				"norm_adv": False,
 
-				"network_update_interval": 100,
+				"network_update_interval": 1,
 			}
 		
 		seeds = [42, 142, 242, 342, 442]
