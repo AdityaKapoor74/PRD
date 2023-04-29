ba9ec34aafea90d3c4a6a61666c494a2677e0422
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-29 21:50:46 +0530

    Update main_ppo.py

--- Agent_MPE/PRD_old/main_ppo.py
+++ Agent_MPE/PRD_old/main_ppo.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
 				# ACTOR
 				"grad_clip_actor": 10.0,
 				"policy_clip": 0.05,
-				"policy_lr": 7e-5, #prd 1e-4
+				"policy_lr": 6e-5, #prd 1e-4
 				"policy_weight_decay": 5e-4,
 				"entropy_pen": 7e-1, #8e-3
 				"gae_lambda": 0.95,
