7b0952d0062f4876a36f288e6590dabb0feb62cd
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-04-29 22:30:03 +0530

    Update main_ppo.py

--- Agent_MPE/PRD_old/main_ppo.py
+++ Agent_MPE/PRD_old/main_ppo.py
@@ -83,11 +83,11 @@ if __name__ == '__main__':
 				"policy_clip": 0.05,
 				"policy_lr": 5e-5, #prd 1e-4
 				"policy_weight_decay": 5e-4,
-				"entropy_pen": 6.5e-1, #8e-3
+				"entropy_pen": 7e-1, #8e-3
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.1,
+				"threshold_max": 0.05,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": False,
