e52435d972dbbade4a2ef91e97a2fe77e14c29ae
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-01 10:53:09 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "PRESSURE PLATE"
 		env_name = "pressureplate-linear-6p-v0"
-		experiment_type = "shared" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
 
 		dictionary = {
 				# TRAINING
@@ -271,11 +271,11 @@ if __name__ == '__main__':
 				"env": env_name,
 
 				# CRITIC
-				"q_value_lr": 3e-4, #1e-3
-				"value_lr": 3e-4, #1e-3
+				"q_value_lr": 5e-4, #1e-3
+				"value_lr": 5e-4, #1e-3
 				"q_weight_decay": 5e-4,
 				"v_weight_decay": 5e-4,
-				"grad_clip_critic": 0.5,
+				"grad_clip_critic": 10.0,
 				"value_clip": 0.05,
 				"enable_hard_attention": False,
 				"num_heads": 4,
@@ -286,11 +286,13 @@ if __name__ == '__main__':
 				
 
 				# ACTOR
-				"grad_clip_actor": 0.5,
+				"grad_clip_actor": 10.0,
 				"policy_clip": 0.05,
 				"policy_lr": 1e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
-				"entropy_pen": 8e-3, #8e-3
+				"entropy_pen": 5e-2, #8e-3
+				"entropy_final": 0.0,
+				"entropy_delta_episodes": 15000,
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
