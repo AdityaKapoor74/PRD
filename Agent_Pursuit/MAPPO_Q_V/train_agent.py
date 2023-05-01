fb13dc8f9218a4742fb623f6fd701b30dafaa887
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-01 20:31:47 +0530

    Update train_agent.py

--- Agent_Pursuit/MAPPO_Q_V/train_agent.py
+++ Agent_Pursuit/MAPPO_Q_V/train_agent.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "PURSUIT"
 		env_name = "pursuit_v4"
-		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay
+		experiment_type = "prd_above_threshold" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay
 
 		max_time_steps = 500
 		num_agents = 8
@@ -272,7 +272,7 @@ if __name__ == '__main__':
 				"v_weight_decay": 5e-4,
 				"grad_clip_critic": 10.0,
 				"value_clip": 0.2,
-				"enable_hard_attention": False,
+				"enable_hard_attention": True,
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
 				"critic_score_regularizer": 0.0,
