071554ea1a984c0ff925418d982ab5b8bcec9b85
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-02 19:24:12 +0530

    Add files via upload

---
+++ Agent_Pursuit/main_coma.py
@@ -0,0 +1,63 @@
+from macoma import MACOMA
+from pettingzoo.sisl import pursuit_v4
+import torch
+
+
+if __name__ == '__main__':
+
+	for i in range(1,6):
+		extension = "COMA_run"+str(i)
+		test_num = "PURSUIT"
+		env_name = "pursuit_v4"
+
+		max_time_steps = 500
+		num_agents = 8
+		num_actions = 5
+
+		dictionary = {
+				"critic_dir": "../../../tests/COMA_"+env_name+"/models/critic_networks/run"+str(i)+"/",
+				"actor_dir": "../../../tests/COMA_"+env_name+"/models/actor_networks/run"+str(i)+"/",
+				"gif_dir": "../../../tests/COMA_"+env_name+"/gif_dir/run"+str(i)+"/",
+				"policy_eval_dir":"../../../tests/COMA_"+env_name+"/policy_eval_dir/run"+str(i)+"/",
+				"env": env_name, 
+				"test_num":test_num,
+				"extension":extension,
+				"iteration": i,
+				"value_lr": 5e-4, 
+				"policy_lr": 1e-4,
+				"grad_clip_critic": 10.0,
+				"grad_clip_actor": 10.0,
+				"critic_entropy_pen": 0.0,
+				"epsilon_start": 1.0,
+				"epsilon_end": 0.1,
+				"epsilon_episode_steps": 500,
+				"target_critic_update": 100,
+				"gamma": 0.99,
+				"lambda": 0.6,
+				"gif": False,
+				"gif_checkpoint":1,
+				"load_models": False,
+				"model_path_value": " ",
+				"model_path_policy": " ",
+				"eval_policy": True,
+				"save_model": True,
+				"save_model_checkpoint": 1000,
+				"save_comet_ml_plot": True,
+				"learn":True,
+				"max_episodes": 2000,
+				"max_time_steps": max_time_steps,
+				"norm_adv": False,
+				"norm_rew": False,
+			}
+		seeds = [42, 142, 242, 342, 442]
+		torch.manual_seed(seeds[dictionary["iteration"]-1])
+		env = pursuit_v4.parallel_env(max_cycles=max_time_steps, x_size=16, y_size=16, shared_reward=False, n_evaders=30,
+									n_pursuers=num_agents,obs_range=7, n_catch=2, freeze_evaders=False, tag_reward=0.01,
+									catch_reward=5.0, urgency_reward=-0.1, surround=True, constraint_window=1.0)
+		dictionary["height"] = 7
+		dictionary["width"] = 7
+		dictionary["channel"] = 3
+		dictionary["num_agents"] = num_agents
+		dictionary["num_actions"] = num_actions
+		ma_controller = MACOMA(env,dictionary)
+		ma_controller.run()
