4d701baccf9db77c5fe9e9b3aef6547823ed7f88
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-03 14:24:03 +0530

    Update main_ppo.py

--- Agent_MPE/PRD_old/main_ppo.py
+++ Agent_MPE/PRD_old/main_ppo.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
 				"env": env_name,
 
 				# CRITIC
-				"value_lr": 5e-4, #1e-3
+				"value_lr": 1e-4, #1e-3
 				"value_weight_decay": 5e-4,
 				"grad_clip_critic": 10.0,
 				"value_clip": 0.05,
@@ -74,20 +74,20 @@ if __name__ == '__main__':
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
 				"critic_score_regularizer": 0.0,
-				"lambda": 0.6, # 1 --> Monte Carlo; 0 --> TD(1)
+				"lambda": 0.8, # 1 --> Monte Carlo; 0 --> TD(1)
 				"norm_returns": False,
 				
 
 				# ACTOR
 				"grad_clip_actor": 10.0,
 				"policy_clip": 0.05,
-				"policy_lr": 1e-4, #prd 1e-4
+				"policy_lr": 5e-5, #prd 1e-4
 				"policy_weight_decay": 5e-4,
 				"entropy_pen": 7e-1, #8e-3
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.01,
+				"threshold_max": 0.04,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": False,
