4bec920b320ceb366c5a4c86b79ab96f4a877b52
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-04 10:52:18 +0530

    Update train_agent.py

--- Agent_MPE/LICA/train_agent.py
+++ Agent_MPE/LICA/train_agent.py
@@ -289,7 +289,7 @@ if __name__ == '__main__':
 				"scheduler_need": False,
 				"update_episode_interval": 1,
 				"num_updates": 1,
-				"entropy_coeff": 0.11,
+				"entropy_coeff": 4e-2,
 				"lambda": 0.8,
 
 				# ENVIRONMENT
