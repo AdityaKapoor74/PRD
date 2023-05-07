790ee2f2cb91a5de42807ee9116ce77034376526
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-07 14:22:11 +0530

    Update train_agent.py

--- Agent_MPE/MAPPO_Q_V/train_agent.py
+++ Agent_MPE/MAPPO_Q_V/train_agent.py
@@ -290,7 +290,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "TEAM COLLISION AVOIDANCE"
 		env_name = "crossing_team_greedy"
-		experiment_type = "shared" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay
+		experiment_type = "prd_above_threshold_decay" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay
 
 		dictionary = {
 				# TRAINING
@@ -334,7 +334,7 @@ if __name__ == '__main__':
 				"v_weight_decay": 5e-4,
 				"grad_clip_critic": 0.5,
 				"value_clip": 0.05,
-				"enable_hard_attention": True,
+				"enable_hard_attention": False,
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
 				"critic_score_regularizer": 0.0,
@@ -349,9 +349,9 @@ if __name__ == '__main__':
 				"policy_weight_decay": 5e-4,
 				"entropy_pen": 4e-2, #8e-3
 				"gae_lambda": 0.95,
-				"select_above_threshold": 0.0,
+				"select_above_threshold": 0.12,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.12,
+				"threshold_max": 0.0,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": False,
