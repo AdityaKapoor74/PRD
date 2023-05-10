8fab8f96dc9fbe3ea2c808460b47312df79180ff
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-10 14:34:42 +0530

    Update main_ppo.py

--- Agents/PRD_old/main_ppo.py
+++ Agents/PRD_old/main_ppo.py
@@ -14,7 +14,7 @@ PRD_MAA2C_Q: value_lr = 5e-4; policy_lr = 3e-4; entropy_pen = 8e-3; grad_clip_cr
 if __name__ == '__main__':
 	# crossing_greedy/ crossing_fully_coop /  paired_by_sharing_goals/ crossing_partially_coop/ color_social_dilemma
 	for i in range(1,6):
-		extension = "MAA2C_run_"+str(i)
+		extension = "PRD_old_"+str(i)
 		test_num = "PRESSURE PLATE" 
 		env_name = "pressureplate-linear-6p-v0"
 		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_ascend, greedy, shared
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"n_epochs": 5,
+				"n_epochs": 10,
 				"update_ppo_agent": 7, # update ppo agent after every update_ppo_agent episodes
 				"test_num":test_num,
 				"extension":extension,
@@ -54,10 +54,10 @@ if __name__ == '__main__':
 				"env": env_name,
 
 				# CRITIC
-				"value_lr": 3e-4, #1e-3
+				"value_lr": 5e-4, #1e-3
 				"value_weight_decay": 5e-4,
 				"grad_clip_critic": 10.0,
-				"value_clip": 0.05,
+				"value_clip": 0.2,
 				"enable_hard_attention": False,
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
@@ -68,14 +68,14 @@ if __name__ == '__main__':
 
 				# ACTOR
 				"grad_clip_actor": 10.0,
-				"policy_clip": 0.05,
-				"policy_lr": 7e-5, #prd 1e-4
+				"policy_clip": 0.2,
+				"policy_lr": 1e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
 				"entropy_pen": 4e-1, #8e-3
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.01,
+				"threshold_max": 0.1,
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": False,
