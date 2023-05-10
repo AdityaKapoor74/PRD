66d0d73f2c75e14896a6f6ad8be5ef0271b29bb9
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-10 14:20:21 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "PRESSURE PLATE"
 		env_name = "pressureplate-linear-6p-v0"
-		experiment_type = "prd_above_threshold_decay" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
+		experiment_type = "prd_top_2" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
 
 		dictionary = {
 				# TRAINING
@@ -294,11 +294,11 @@ if __name__ == '__main__':
 				"entropy_final": 0.0,
 				"entropy_delta_episodes": 30000,
 				"gae_lambda": 0.95,
-				"select_above_threshold": 0.2,
+				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
 				"threshold_max": 0.0, # 0.2
 				"steps_to_take": 1000,
-				"top_k": 0,
+				"top_k": 2,
 				"norm_adv": False,
 
 				"network_update_interval": 1,
