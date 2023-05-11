ed1646b4d684f57b4a94d6d8572b933a1846f834
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-11 13:15:56 +0530

    Update main_coma.py

--- Agents/COMA/main_coma.py
+++ Agents/COMA/main_coma.py
@@ -1,5 +1,5 @@
 from macoma import MACOMA
-import pressureplate
+import lbforaging
 import gym
 import torch
 
@@ -8,8 +8,13 @@ if __name__ == '__main__':
 
 	for i in range(1,6):
 		extension = "COMA_run"+str(i)
-		test_num = "PRESSURE PLATE"
-		env_name = "pressureplate-linear-6p-v0"
+		test_num = "LB-FORAGING"
+		num_players = 6
+		num_food = 9
+		grid_size = 12
+		fully_coop = False
+		max_episode_steps = 70
+		env_name = "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "")
 
 		dictionary = {
 				"critic_dir": "../../../tests/COMA_"+env_name+"/models/critic_networks/run"+str(i)+"/",
@@ -42,14 +47,14 @@ if __name__ == '__main__':
 				"save_comet_ml_plot": True,
 				"learn":True,
 				"max_episodes": 30000,
-				"max_time_steps": 70,
+				"max_time_steps": max_episode_steps,
 				"norm_adv": False,
 				"norm_rew": False,
 			}
 		seeds = [42, 142, 242, 342, 442]
 		torch.manual_seed(seeds[dictionary["iteration"]-1])
-		env = gym.make(env_name)
-		dictionary["global_observation"] = 133
-		dictionary["local_observation"] = 133
+		env = gym.make(env_name, max_episode_steps=max_episode_steps, penalty=0.01, normalize_reward=True)
+		dictionary["global_observation"] = num_players*3 + num_food*3
+		dictionary["local_observation"] = num_players*3 + num_food*3
 		ma_controller = MACOMA(env,dictionary)
 		ma_controller.run()
