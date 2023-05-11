d2fb0c37ebd033fb17dc69045ab4361dc23d455a
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-11 13:33:11 +0530

    Update main_ppo.py

--- Agents/PRD_old/main_ppo.py
+++ Agents/PRD_old/main_ppo.py
@@ -1,5 +1,5 @@
 from mappo import MAPPO
-import pressureplate
+import lbforaging
 import gym
 import torch
 
@@ -15,8 +15,13 @@ if __name__ == '__main__':
 	# crossing_greedy/ crossing_fully_coop /  paired_by_sharing_goals/ crossing_partially_coop/ color_social_dilemma
 	for i in range(1,6):
 		extension = "PRD_old_"+str(i)
-		test_num = "PRESSURE PLATE" 
-		env_name = "pressureplate-linear-6p-v0"
+		test_num = "LB-FORAGING"
+		num_players = 6
+		num_food = 9
+		grid_size = 12
+		fully_coop = False
+		max_episode_steps = 70
+		env_name = "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "")
 		experiment_type = "prd_above_threshold_ascend" # prd_above_threshold_ascend, greedy, shared
 
 		dictionary = {
@@ -85,8 +90,8 @@ if __name__ == '__main__':
 
 		seeds = [42, 142, 242, 342, 442]
 		torch.manual_seed(seeds[dictionary["iteration"]-1])
-		env = gym.make(env_name)
-		dictionary["global_observation"] = 133
-		dictionary["local_observation"] = 133
+		env = gym.make(env_name, max_episode_steps=max_episode_steps, penalty=0.01, normalize_reward=True)
+		dictionary["global_observation"] = num_players*3 + num_food*3
+		dictionary["local_observation"] = num_players*3 + num_food*3
 		ma_controller = MAPPO(env,dictionary)
 		ma_controller.run()
