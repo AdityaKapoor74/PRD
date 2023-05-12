9a52c8256c11dcb1ce56faf44174e766b6d90ec0
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-12 16:38:07 +0530

    Update main_ppo.py

--- Agents/PRD_old/main_ppo.py
+++ Agents/PRD_old/main_ppo.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"n_epochs": 10,
+				"n_epochs": 1,
 				"update_ppo_agent": 7, # update ppo agent after every update_ppo_agent episodes
 				"test_num":test_num,
 				"extension":extension,
@@ -48,7 +48,7 @@ if __name__ == '__main__':
 				"save_model_checkpoint": 1000,
 				"save_comet_ml_plot": True,
 				"learn":True,
-				"max_episodes": 30000,
+				"max_episodes": 10000,
 				"max_time_steps": max_episode_steps,
 				"experiment_type": experiment_type,
 				"parallel_training": False,
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 				"env": env_name,
 
 				# CRITIC
-				"value_lr": 1e-4, #1e-3
+				"value_lr": 5e-3, #1e-3
 				"value_weight_decay": 5e-4,
 				"grad_clip_critic": 0.5,
 				"value_clip": 0.2,
@@ -74,9 +74,9 @@ if __name__ == '__main__':
 				# ACTOR
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.2,
-				"policy_lr": 1e-4, #prd 1e-4
+				"policy_lr": 1e-3, #prd 1e-4
 				"policy_weight_decay": 5e-4,
-				"entropy_pen": 1e-2, #8e-3
+				"entropy_pen": 1e-3, #8e-3
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
@@ -90,7 +90,7 @@ if __name__ == '__main__':
 
 		seeds = [42, 142, 242, 342, 442]
 		torch.manual_seed(seeds[dictionary["iteration"]-1])
-		env = gym.make(env_name, max_episode_steps=max_episode_steps, penalty=0.01, normalize_reward=True)
+		env = gym.make(env_name, max_episode_steps=max_episode_steps, penalty=0.0, normalize_reward=True)
 		dictionary["global_observation"] = num_players*3 + num_food*3
 		dictionary["local_observation"] = num_players*3 + num_food*3
 		ma_controller = MAPPO(env,dictionary)
