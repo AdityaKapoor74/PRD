aee44c02619fa460f49b5eef1fd2944c681c3182
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-13 11:27:23 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -252,7 +252,7 @@ if __name__ == '__main__':
 		fully_coop = False
 		max_episode_steps = 70
 		env_name = "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "")
-		experiment_type = "shared" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
 		
 
 		dictionary = {
@@ -315,8 +315,8 @@ if __name__ == '__main__':
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.2, # 0.2
-				"steps_to_take": 1000,
+				"threshold_max": 0.33, # 0.2
+				"steps_to_take": 300,
 				"top_k": 0,
 				"norm_adv": False,
 
