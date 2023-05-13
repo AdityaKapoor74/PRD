68abf7efb3facc56081359b079d72b31517150cb
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-13 16:30:04 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -29,7 +29,7 @@ class MAPPO:
 		self.gif_checkpoint = dictionary["gif_checkpoint"]
 		self.eval_policy = dictionary["eval_policy"]
 		self.num_agents = dictionary["num_agents"]
-		self.num_actions = self.env.action_space.n
+		self.num_actions = 19
 		self.date_time = f"{datetime.datetime.now():%d-%m-%Y}"
 		self.env_name = dictionary["env"]
 		self.test_num = dictionary["test_num"]
