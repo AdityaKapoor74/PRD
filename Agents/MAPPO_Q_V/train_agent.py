ac0d54fd533572b2b9e947b2a7bd0e125680b3cd
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-13 16:42:02 +0530

    Update train_agent.py

--- Agents/MAPPO_Q_V/train_agent.py
+++ Agents/MAPPO_Q_V/train_agent.py
@@ -233,7 +233,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "GOOGLE FOOTBALL"
 		env_name = "academy_counterattack_easy"
-		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
+		experiment_type = "shared" # shared, prd_above_threshold, prd_above_threshold_ascend, prd_top_k, prd_above_threshold_decay
 
 		dictionary = {
 				# TRAINING
@@ -244,8 +244,8 @@ if __name__ == '__main__':
 				"actor_dir": '../../../tests/'+test_num+'/models/'+env_name+'_'+experiment_type+'_'+extension+'/actor_networks/',
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
-				"n_epochs": 5,
-				"update_ppo_agent": 7, # update ppo agent after every update_ppo_agent episodes
+				"n_epochs": 1,
+				"update_ppo_agent": 1, # update ppo agent after every update_ppo_agent episodes
 				"test_num":test_num,
 				"extension":extension,
 				"gamma": 0.99,
@@ -290,7 +290,7 @@ if __name__ == '__main__':
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
-				"entropy_pen": 5e-2, #8e-3
+				"entropy_pen": 0.0, #8e-3
 				"entropy_final": 0.0,
 				"entropy_delta_episodes": 30000,
 				"gae_lambda": 0.95,
