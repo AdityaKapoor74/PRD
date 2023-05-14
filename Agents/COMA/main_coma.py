924a30c59b09eab2768936033f09fb8460779208
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-14 15:14:19 +0530

    Update main_coma.py

--- Agents/COMA/main_coma.py
+++ Agents/COMA/main_coma.py
@@ -1,6 +1,5 @@
 from macoma import MACOMA
-import pressureplate
-import gym
+import gfootball.env as football_env
 import torch
 
 
@@ -8,8 +7,8 @@ if __name__ == '__main__':
 
 	for i in range(1,6):
 		extension = "COMA_run"+str(i)
-		test_num = "PRESSURE PLATE"
-		env_name = "pressureplate-linear-6p-v0"
+		test_num = "GOOGLE FOOTBALL"
+		env_name = "academy_counterattack_easy"
 
 		dictionary = {
 				"critic_dir": "../../../tests/COMA_"+env_name+"/models/critic_networks/run"+str(i)+"/",
@@ -20,15 +19,15 @@ if __name__ == '__main__':
 				"test_num":test_num,
 				"extension":extension,
 				"iteration": i,
-				"value_lr": 5e-4, 
-				"policy_lr": 1e-4,
-				"grad_clip_critic": 10.0,
-				"grad_clip_actor": 10.0,
+				"value_lr": 1e-3, 
+				"policy_lr": 5e-4,
+				"grad_clip_critic": 0.5,
+				"grad_clip_actor": 0.5,
 				"critic_entropy_pen": 0.0,
 				"epsilon_start": 1.0,
 				"epsilon_end": 0.05,
-				"epsilon_episode_steps": 30000,
-				"target_critic_update": 100,
+				"epsilon_episode_steps": 5000,
+				"target_critic_update": 200,
 				"gamma": 0.99,
 				"lambda": 0.8,
 				"gif": False,
@@ -42,14 +41,27 @@ if __name__ == '__main__':
 				"save_comet_ml_plot": True,
 				"learn":True,
 				"max_episodes": 30000,
-				"max_time_steps": 70,
+				"max_time_steps": 40,
 				"norm_adv": False,
 				"norm_rew": False,
+				"num_agents": 6,
 			}
 		seeds = [42, 142, 242, 342, 442]
 		torch.manual_seed(seeds[dictionary["iteration"]-1])
-		env = gym.make(env_name)
-		dictionary["global_observation"] = 133
-		dictionary["local_observation"] = 133
+		env = football_env.create_environment(
+			env_name=env_name,
+			number_of_left_players_agent_controls=dictionary["num_agents"],
+			# number_of_right_players_agent_controls=2,
+			representation="simple115",
+			# num_agents=4,
+			stacked=False, 
+			logdir='/tmp/football', 
+			write_goal_dumps=False, 
+			write_full_episode_dumps=False, 
+			rewards='scoring,checkpoints',
+			render=False
+			)
+		dictionary["global_observation"] = 115
+		dictionary["local_observation"] = 115
 		ma_controller = MACOMA(env,dictionary)
 		ma_controller.run()
