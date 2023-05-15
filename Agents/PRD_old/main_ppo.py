553207a0d4f02d2c669e4a2d4a0b1b7cabd9d3b5
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-05-15 12:57:41 +0530

    Update main_ppo.py

--- Agents/PRD_old/main_ppo.py
+++ Agents/PRD_old/main_ppo.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
 				# CRITIC
 				"value_lr": 1e-4, #1e-3
 				"value_weight_decay": 5e-4,
-				"grad_clip_critic": 0.5,
+				"grad_clip_critic": 10.0,
 				"value_clip": 0.2,
 				"enable_hard_attention": False,
 				"num_heads": 4,
@@ -67,7 +67,7 @@ if __name__ == '__main__':
 				
 
 				# ACTOR
-				"grad_clip_actor": 0.5,
+				"grad_clip_actor": 10.0,
 				"policy_clip": 0.2,
 				"policy_lr": 1e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
