aa8301283b98323f9aab5dedb2ea6be2aff9b7cb
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-10 18:28:33 +0530

    Add files via upload

---
+++ Agent/PRD_old/ppo_model.py
@@ -0,0 +1,164 @@
+from typing import Any, List, Tuple, Union
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import datetime
+import math
+
+
+class MLP_Policy(nn.Module):
+	def __init__(self, obs_input_dim, num_actions, num_agents, device):
+		super(MLP_Policy, self).__init__()
+
+		self.name = "MLP Policy"
+
+		self.rnn_hidden_state = None
+		self.num_agents = num_agents
+		self.num_actions = num_actions
+		self.device = device
+		self.Layer_1 = nn.Sequential(nn.Linear(obs_input_dim, 128), nn.GELU())
+		self.RNN = nn.GRUCell(input_size=128, hidden_size=128)
+		self.Layer_2 = nn.Sequential(nn.Linear(128, 64), nn.GELU(), nn.Linear(64, num_actions))
+
+		self.reset_parameters()
+
+	def reset_parameters(self):
+
+		nn.init.xavier_uniform_(self.Layer_1[0].weight)
+		nn.init.xavier_uniform_(self.Layer_2[0].weight)
+		nn.init.xavier_uniform_(self.Layer_2[2].weight)
+
+
+	def forward(self, local_observations, mask_actions=None):
+		intermediate = self.Layer_1(local_observations)
+		self.rnn_hidden_state = self.RNN(intermediate.view(-1, intermediate.shape[-1])).view(*intermediate.shape)
+		policy = self.Layer_2(self.rnn_hidden_state) + mask_actions
+		return F.softmax(policy, dim=-1), self.rnn_hidden_state
+
+
+class TransformerCritic(nn.Module):
+	'''
+	https://proceedings.neurips.cc/paper/2017/file/3f5ee243547dee91fbd053c1c4a845aa-Paper.pdf
+	'''
+	def __init__(self, obs_input_dim, obs_act_input_dim, num_agents, num_actions, device):
+		super(TransformerCritic, self).__init__()
+		
+		self.name = "TransformerCritic"
+
+		self.num_agents = num_agents
+		self.num_actions = num_actions
+		self.device = device
+
+		self.state_embed = nn.Sequential(
+			nn.Linear(obs_input_dim, 64, bias=True), 
+			nn.GELU(),
+			)
+		self.key = nn.Sequential(
+			nn.Linear(64, 64, bias=True),
+			nn.GELU(),
+			)
+		self.query = nn.Sequential(
+			nn.Linear(64, 64, bias=True),
+			nn.GELU(),
+			)
+		self.state_act_pol_embed = nn.Sequential(
+			nn.Linear(obs_act_input_dim, 64, bias=True), 
+			nn.GELU(),
+			)
+		self.attention_value = nn.Sequential(
+			nn.Linear(64, 64, bias=True), 
+			nn.GELU(),
+			)
+
+		# dimesion of key
+		self.d_k_obs_act = 64 
+
+		self.layer_norm_state_act_pol_embed = nn.LayerNorm(64) 
+
+		self.curr_agent_state_embed = nn.Sequential(
+			nn.Linear(obs_input_dim, 64, bias=True), 
+			nn.GELU(),
+			)
+		self.layer_norm_state_embed = nn.LayerNorm(64)
+
+		# ********************************************************************************************************
+
+		# ********************************************************************************************************
+		# FCN FINAL LAYER TO GET VALUES
+		self.final_value_layers = nn.Sequential(
+			nn.Linear(64*2, 64, bias=True), 
+			nn.GELU(),
+			nn.Linear(64, 64, bias=True), 
+			nn.GELU(),
+			nn.Linear(64, 1)
+			)
+			
+		# ********************************************************************************************************	
+
+		self.place_policies = torch.zeros(self.num_agents,self.num_agents,obs_act_input_dim).to(self.device)
+		self.place_actions = torch.ones(self.num_agents,self.num_agents,obs_act_input_dim).to(self.device)
+		one_hots = torch.ones(obs_act_input_dim)
+		zero_hots = torch.zeros(obs_act_input_dim)
+
+		for j in range(self.num_agents):
+			self.place_policies[j][j] = one_hots
+			self.place_actions[j][j] = zero_hots
+
+
+		self.reset_parameters()
+
+	def reset_parameters(self):
+		"""Reinitialize learnable parameters."""
+
+		# EMBEDDINGS
+		nn.init.xavier_uniform_(self.state_embed[0].weight)
+		nn.init.xavier_uniform_(self.state_act_pol_embed[0].weight)
+
+		nn.init.xavier_uniform_(self.key[0].weight)
+		nn.init.xavier_uniform_(self.query[0].weight)
+		nn.init.xavier_uniform_(self.attention_value[0].weight)
+
+		nn.init.xavier_uniform_(self.curr_agent_state_embed[0].weight)
+
+		nn.init.xavier_uniform_(self.final_value_layers[0].weight)
+		nn.init.xavier_uniform_(self.final_value_layers[2].weight)
+		nn.init.xavier_uniform_(self.final_value_layers[4].weight)
+
+
+
+	def forward(self, states, policies, actions):
+		# EMBED STATES
+		states_embed = self.state_embed(states)
+		# KEYS
+		key_obs = self.key(states_embed)
+		# QUERIES
+		query_obs = self.query(states_embed)
+		# WEIGHT
+		weight = F.softmax(torch.matmul(query_obs,key_obs.transpose(1,2))/math.sqrt(self.d_k_obs_act),dim=-1)
+		ret_weight = weight
+
+		obs_actions = torch.cat([states,actions],dim=-1)
+		obs_policy = torch.cat([states,policies], dim=-1)
+		obs_actions = obs_actions.repeat(1,self.num_agents,1).reshape(obs_actions.shape[0],self.num_agents,self.num_agents,-1)
+		obs_policy = obs_policy.repeat(1,self.num_agents,1).reshape(obs_policy.shape[0],self.num_agents,self.num_agents,-1)
+		obs_actions_policies = self.place_policies*obs_policy + self.place_actions*obs_actions
+		# EMBED STATE ACTION POLICY
+		obs_actions_policies_embed = self.state_act_pol_embed(obs_actions_policies)
+		attention_values = self.attention_value(obs_actions_policies_embed)
+
+		attention_values = attention_values.repeat(1,self.num_agents,1,1).reshape(attention_values.shape[0],self.num_agents,self.num_agents,self.num_agents,-1)
+
+		weight = weight.unsqueeze(-2).repeat(1,1,self.num_agents,1).unsqueeze(-1)
+		weighted_attention_values = attention_values*weight
+		node_features = torch.sum(weighted_attention_values, dim=-2)
+		node_features = self.layer_norm_state_act_pol_embed(obs_actions_policies_embed+node_features)
+
+		curr_agent_state_embed = self.curr_agent_state_embed(states)
+		curr_agent_state_embed = self.layer_norm_state_embed(curr_agent_state_embed+states_embed)
+		curr_agent_node_features = torch.cat([curr_agent_state_embed.unsqueeze(-2).repeat(1,1,self.num_agents,1), node_features], dim=-1)
+
+		Value = self.final_value_layers(curr_agent_node_features)
+
+		return Value, ret_weight
---
+++ Agent/PRD_old/utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class RolloutBuffer:
+	def __init__(self, num_episodes, max_time_steps, num_agents, obs_shape_critic, obs_shape_actor, num_actions, rnn_hidden_actor):
+		self.num_episodes = num_episodes
+		self.max_time_steps = max_time_steps
+		self.num_agents = num_agents
+		self.obs_shape_critic = obs_shape_critic
+		self.obs_shape_actor = obs_shape_actor
+		self.num_actions = num_actions
+		self.rnn_hidden_actor = rnn_hidden_actor
+		self.episode_num = 0
+		self.time_step = 0
+
+		self.states_critic = np.zeros((num_episodes, max_time_steps, num_agents, obs_shape_critic))
+		self.states_actor = np.zeros((num_episodes, max_time_steps, num_agents, obs_shape_actor))
+		self.rnn_hidden_state_actor = np.zeros((num_episodes, max_time_steps, num_agents, rnn_hidden_actor))
+		self.actions = np.zeros((num_episodes, max_time_steps, num_agents), dtype=int)
+		self.one_hot_actions = np.zeros((num_episodes, max_time_steps, num_agents, num_actions))
+		self.mask_actions = np.zeros((num_episodes, max_time_steps, num_agents, num_actions))
+		self.rewards = np.zeros((num_episodes, max_time_steps, num_agents))
+		self.dones = np.zeros((num_episodes, max_time_steps, num_agents))
+		self.masks = np.zeros((num_episodes, max_time_steps))
+	
+
+	def clear(self):
+		self.states_critic = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.obs_shape_critic))
+		self.states_actor = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.obs_shape_actor))
+		self.rnn_hidden_state_actor = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.rnn_hidden_actor))
+		self.actions = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents), dtype=int)
+		self.one_hot_actions = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.num_actions))
+		self.mask_actions = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.num_actions))
+		self.rewards = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents))
+		self.dones = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents))
+		self.masks = np.zeros((self.num_episodes, self.max_time_steps))
+
+		self.time_step = 0
+		self.episode_num = 0
+
+	def push(self, state_critic, state_actor, rnn_hidden_actor, actions, one_hot_actions, mask_actions, rewards, dones):
+
+		self.states_critic[self.episode_num][self.time_step] = state_critic
+		self.states_actor[self.episode_num][self.time_step] = state_actor
+		self.rnn_hidden_state_actor[self.episode_num][self.time_step] = rnn_hidden_actor
+		self.actions[self.episode_num][self.time_step] = actions
+		self.one_hot_actions[self.episode_num][self.time_step] = one_hot_actions
+		self.mask_actions[self.episode_num][self.time_step] = mask_actions
+		self.rewards[self.episode_num][self.time_step] = rewards
+		self.dones[self.episode_num][self.time_step] = dones
+		self.masks[self.episode_num][self.time_step] = 1.
+
+		if self.time_step < self.max_time_steps-1:
+			self.time_step += 1
+		else:
+			self.episode_num += 1
+			self.time_step = 0
