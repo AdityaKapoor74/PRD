e6796eea4a041fc086db38662e15c3768e5bb8a1
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-10 17:56:53 +0530

    Update train_agent.py

--- Agent/MAPPO_Q_V/train_agent.py
+++ Agent/MAPPO_Q_V/train_agent.py
@@ -233,7 +233,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
 		env_name = "10m_vs_11m"
-		experiment_type = "prd_above_threshold" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
+		experiment_type = "prd_soft_advantage" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
 				# TRAINING
@@ -290,11 +290,11 @@ if __name__ == '__main__':
 				"enable_grad_clip_actor": False,
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.05,
-				"policy_lr": 1e-4, #prd 1e-4
+				"policy_lr": 1e-3, #prd 1e-4
 				"policy_weight_decay": 5e-4,
 				"entropy_pen": 1e-2, #8e-3
 				"gae_lambda": 0.95,
-				"select_above_threshold": 0.05,
+				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
 				"threshold_max": 0.0, #0.12
 				"steps_to_take": 1000,
