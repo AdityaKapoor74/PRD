01b9f444141fd4f6cf719a32fa9b2054aca3dc1f
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-11 13:00:41 +0530

    Update train_agent.py

--- Agent/MAPPO_Q_V/train_agent.py
+++ Agent/MAPPO_Q_V/train_agent.py
@@ -233,7 +233,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
 		env_name = "10m_vs_11m"
-		experiment_type = "prd_above_threshold" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
+		experiment_type = "shared" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
 				# TRAINING
@@ -277,7 +277,7 @@ if __name__ == '__main__':
 				"enable_grad_clip_critic": False,
 				"grad_clip_critic": 0.5,
 				"value_clip": 0.05,
-				"enable_hard_attention": False,
+				"enable_hard_attention": True,
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
 				"critic_score_regularizer": 0.0,
@@ -296,7 +296,7 @@ if __name__ == '__main__':
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
-				"threshold_max": 0.1, #0.12
+				"threshold_max": 0.0, #0.12
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": False,
