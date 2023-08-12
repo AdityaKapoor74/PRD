4829d5e693b0a9ef5837a1acaeb07b30eaa2e95c
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-12 16:53:32 +0530

    Update utils.py

--- Agent/PRD_old/utils.py
+++ Agent/PRD_old/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class RolloutBuffer:
-	def __init__(self, num_episodes, max_time_steps, num_agents, obs_shape_critic, obs_shape_actor, num_actions, rnn_hidden_actor):
+	def __init__(self, num_episodes, max_time_steps, num_agents, obs_shape_critic, obs_shape_actor, num_actions, rnn_hidden_actor, rnn_hidden_critic):
 		self.num_episodes = num_episodes
 		self.max_time_steps = max_time_steps
 		self.num_agents = num_agents
@@ -9,10 +9,12 @@ class RolloutBuffer:
 		self.obs_shape_actor = obs_shape_actor
 		self.num_actions = num_actions
 		self.rnn_hidden_actor = rnn_hidden_actor
+		self.rnn_hidden_critic = rnn_hidden_critic
 		self.episode_num = 0
 		self.time_step = 0
 
 		self.states_critic = np.zeros((num_episodes, max_time_steps, num_agents, obs_shape_critic))
+		self.rnn_hidden_state_critic = np.zeros((num_episodes, max_time_steps, num_agents, num_agents, rnn_hidden_critic))
 		self.states_actor = np.zeros((num_episodes, max_time_steps, num_agents, obs_shape_actor))
 		self.rnn_hidden_state_actor = np.zeros((num_episodes, max_time_steps, num_agents, rnn_hidden_actor))
 		self.actions = np.zeros((num_episodes, max_time_steps, num_agents), dtype=int)
@@ -25,6 +27,7 @@ class RolloutBuffer:
 
 	def clear(self):
 		self.states_critic = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.obs_shape_critic))
+		self.rnn_hidden_state_critic = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.num_agents, self.rnn_hidden_critic))
 		self.states_actor = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.obs_shape_actor))
 		self.rnn_hidden_state_actor = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents, self.rnn_hidden_actor))
 		self.actions = np.zeros((self.num_episodes, self.max_time_steps, self.num_agents), dtype=int)
@@ -37,9 +40,10 @@ class RolloutBuffer:
 		self.time_step = 0
 		self.episode_num = 0
 
-	def push(self, state_critic, state_actor, rnn_hidden_actor, actions, one_hot_actions, mask_actions, rewards, dones):
+	def push(self, state_critic, rnn_hidden_critic, state_actor, rnn_hidden_actor, actions, one_hot_actions, mask_actions, rewards, dones):
 
 		self.states_critic[self.episode_num][self.time_step] = state_critic
+		self.rnn_hidden_state_critic[self.episode_num][self.time_step] = rnn_hidden_critic
 		self.states_actor[self.episode_num][self.time_step] = state_actor
 		self.rnn_hidden_state_actor[self.episode_num][self.time_step] = rnn_hidden_actor
 		self.actions[self.episode_num][self.time_step] = actions
