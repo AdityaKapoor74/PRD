e6268af1b2e58477498e6d2d26c12ef7b05269a4
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-14 02:26:36 +0530

    Update train_agent.py

--- Agent/MAPPO_Q_V/train_agent.py
+++ Agent/MAPPO_Q_V/train_agent.py
@@ -287,17 +287,17 @@ if __name__ == '__main__':
 				# CRITIC
 				"rnn_hidden_q": 128,
 				"rnn_hidden_v": 128,				
-				"q_value_lr": 1e-3, #1e-3
-				"v_value_lr": 1e-3, #1e-3
+				"q_value_lr": 5e-4, #1e-3
+				"v_value_lr": 5e-4, #1e-3
 				"q_weight_decay": 5e-4,
 				"v_weight_decay": 5e-4,
 				"enable_grad_clip_critic": False,
 				"grad_clip_critic": 0.5,
-				"value_clip": 0.05,
+				"value_clip": 0.2,
 				"enable_hard_attention": False,
 				"num_heads": 4,
 				"critic_weight_entropy_pen": 0.0,
-				"critic_score_regularizer": 5e-4,
+				"critic_score_regularizer": 0.0,
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
 				"norm_returns": False,
 				
@@ -306,10 +306,10 @@ if __name__ == '__main__':
 				"rnn_hidden_actor": 128,
 				"enable_grad_clip_actor": False,
 				"grad_clip_actor": 0.5,
-				"policy_clip": 0.05,
-				"policy_lr": 1e-3, #prd 1e-4
+				"policy_clip": 0.2,
+				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 5e-4,
-				"entropy_pen": 5e-2, #8e-3
+				"entropy_pen": 1e-2, #8e-3
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
