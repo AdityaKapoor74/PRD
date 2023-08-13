2e56a48e3a004f57b2834f3ee4824e9c14b6c4f5
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-13 22:45:12 +0530

    Update train_agent.py

--- Agent/MAPPO_Q_V/train_agent.py
+++ Agent/MAPPO_Q_V/train_agent.py
@@ -256,7 +256,7 @@ if __name__ == '__main__':
 				"gif_dir": '../../../tests/'+test_num+'/gifs/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"n_epochs": 5,
-				"update_ppo_agent": 10, # update ppo agent after every update_ppo_agent episodes
+				"update_ppo_agent": 5, # update ppo agent after every update_ppo_agent episodes
 				"test_num":test_num,
 				"extension":extension,
 				"gamma": 0.99,
@@ -309,7 +309,7 @@ if __name__ == '__main__':
 				"policy_clip": 0.05,
 				"policy_lr": 1e-3, #prd 1e-4
 				"policy_weight_decay": 5e-4,
-				"entropy_pen": 1e-2, #8e-3
+				"entropy_pen": 5e-2, #8e-3
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0,
 				"threshold_min": 0.0, 
