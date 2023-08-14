f900cae020d74dd7582efc29d2e1e8fb386238db
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-14 15:03:09 +0530

    Update model.py

--- Agent/MAPPO_Q_V/model.py
+++ Agent/MAPPO_Q_V/model.py
@@ -16,9 +16,9 @@ class MLP_Policy(nn.Module):
 		self.num_agents = num_agents
 		self.num_actions = num_actions
 		self.device = device
-		self.Layer_1 = nn.Sequential(nn.Linear(obs_input_dim, 128), nn.GELU())
-		self.RNN = nn.GRUCell(input_size=128, hidden_size=128)
-		self.Layer_2 = nn.Sequential(nn.Linear(128, 64), nn.GELU(), nn.Linear(64, num_actions))
+		self.Layer_1 = nn.Sequential(nn.Linear(obs_input_dim, 64), nn.GELU())
+		self.RNN = nn.GRUCell(input_size=564, hidden_size=64)
+		self.Layer_2 = nn.Sequential(nn.Linear(64, 64), nn.GELU(), nn.Linear(64, num_actions))
 
 		self.reset_parameters()
 
@@ -67,63 +67,63 @@ class Q_network(nn.Module):
 
 		# Embedding Networks
 		self.state_embed = nn.Sequential(
-			nn.Linear(obs_input_dim, 128, bias=True), 
+			nn.Linear(obs_input_dim, 64, bias=True), 
 			nn.GELU(),
 			)
 		self.state_act_embed = nn.Sequential(
-			nn.Linear(obs_input_dim+self.num_actions, 128, bias=True), 
+			nn.Linear(obs_input_dim+self.num_actions, 64, bias=True), 
 			nn.GELU(),
 			)
 
 		# Key, Query, Attention Value, Hard Attention Networks
-		assert 128%self.num_heads == 0
+		assert 64%self.num_heads == 0
 		self.key = nn.ModuleList([nn.Sequential(
-					nn.Linear(128, 128, bias=True), 
+					nn.Linear(64, 64, bias=True), 
 					nn.GELU()
 					).to(self.device) for _ in range(self.num_heads)])
 		self.query = nn.ModuleList([nn.Sequential(
-					nn.Linear(128, 128, bias=True), 
+					nn.Linear(64, 64, bias=True), 
 					nn.GELU()
 					).to(self.device) for _ in range(self.num_heads)])
 		self.attention_value = nn.ModuleList([nn.Sequential(
-					nn.Linear(128, 128//self.num_heads, bias=True), 
+					nn.Linear(64, 64//self.num_heads, bias=True), 
 					nn.GELU()
 					).to(self.device) for _ in range(self.num_heads)])
 
-		self.attention_value_layer_norm = nn.LayerNorm(128)
+		self.attention_value_layer_norm = nn.LayerNorm(64)
 
 		self.attention_value_linear = nn.Sequential(
-			nn.Linear(128, 128),
+			nn.Linear(64, 64),
 			nn.GELU(),
 			)
 
-		self.attention_value_linear_layer_norm = nn.LayerNorm(128)
+		self.attention_value_linear_layer_norm = nn.LayerNorm(64)
 
 		if self.enable_hard_attention:
 			self.hard_attention = nn.ModuleList([nn.Sequential(
-						nn.Linear(128*2, 128//self.num_heads),
+						nn.Linear(64*2, 64//self.num_heads),
 						nn.GELU(),
 						).to(self.device) for _ in range(self.num_heads)])
 
 			self.hard_attention_linear = nn.Sequential(
-				nn.Linear(128, 2)
+				nn.Linear(64, 2)
 				)
 
 
 		# dimesion of key
-		self.d_k = 128
+		self.d_k = 64
 
 		# FCN FINAL LAYER TO GET Q-VALUES
 		self.common_layer = nn.Sequential(
-			nn.Linear(128*2, 128, bias=True), 
+			nn.Linear(64*2, 64, bias=True), 
 			nn.GELU(),
 			)
-		self.RNN = nn.GRUCell(128, 128)
+		self.RNN = nn.GRUCell(64, 64)
 		self.rnn_hidden_state = None
 		self.q_value_layer = nn.Sequential(
-			nn.Linear(128, 128, bias=True),
+			nn.Linear(64, 64, bias=True),
 			nn.GELU(),
-			nn.Linear(128, self.num_actions)
+			nn.Linear(64, self.num_actions)
 			)
 			
 		# ********************************************************************************************************
@@ -289,63 +289,63 @@ class V_network(nn.Module):
 
 		# Embedding Networks
 		self.state_embed = nn.Sequential(
-			nn.Linear(obs_input_dim, 128, bias=True), 
+			nn.Linear(obs_input_dim, 64, bias=True), 
 			nn.GELU(),
 			)
 		self.state_act_embed = nn.Sequential(
-			nn.Linear(obs_input_dim+self.num_actions, 128, bias=True), 
+			nn.Linear(obs_input_dim+self.num_actions, 64, bias=True), 
 			nn.GELU(),
 			)
 
 		# Key, Query, Attention Value, Hard Attention Networks
-		assert 128%self.num_heads == 0
+		assert 64%self.num_heads == 0
 		self.key = nn.ModuleList([nn.Sequential(
-					nn.Linear(128, 128, bias=True), 
+					nn.Linear(64, 64, bias=True), 
 					nn.GELU()
 					).to(self.device) for _ in range(self.num_heads)])
 		self.query = nn.ModuleList([nn.Sequential(
-					nn.Linear(128, 128, bias=True), 
+					nn.Linear(64, 64, bias=True), 
 					nn.GELU()
 					).to(self.device) for _ in range(self.num_heads)])
 		self.attention_value = nn.ModuleList([nn.Sequential(
-					nn.Linear(128, 128//self.num_heads, bias=True), 
+					nn.Linear(64, 64//self.num_heads, bias=True), 
 					nn.GELU()
 					).to(self.device) for _ in range(self.num_heads)])
 
-		self.attention_value_layer_norm = nn.LayerNorm(128)
+		self.attention_value_layer_norm = nn.LayerNorm(64)
 
 		self.attention_value_linear = nn.Sequential(
-			nn.Linear(128, 128),
+			nn.Linear(64, 64),
 			nn.GELU(),
 			)
 
-		self.attention_value_linear_layer_norm = nn.LayerNorm(128)
+		self.attention_value_linear_layer_norm = nn.LayerNorm(64)
 
 		if self.enable_hard_attention:
 			self.hard_attention = nn.ModuleList([nn.Sequential(
-						nn.Linear(128*2, 128//self.num_heads),
+						nn.Linear(64*2, 64//self.num_heads),
 						nn.GELU(),
 						).to(self.device) for _ in range(self.num_heads)])
 
 			self.hard_attention_linear = nn.Sequential(
-				nn.Linear(128, 2)
+				nn.Linear(64, 2)
 				)
 
 
 		# dimesion of key
-		self.d_k = 128
+		self.d_k = 64
 
 		# FCN FINAL LAYER TO GET Q-VALUES
 		self.common_layer = nn.Sequential(
-			nn.Linear(128*2, 128, bias=True), 
+			nn.Linear(64*2, 64, bias=True), 
 			nn.GELU(),
 			)
-		self.RNN = nn.GRUCell(128, 128)
+		self.RNN = nn.GRUCell(64, 64)
 		self.rnn_hidden_state = None
 		self.v_value_layer = nn.Sequential(
-			nn.Linear(128, 128, bias=True),
+			nn.Linear(64, 64, bias=True),
 			nn.GELU(),
-			nn.Linear(128, 1)
+			nn.Linear(64, 1)
 			)
 			
 		# ********************************************************************************************************
