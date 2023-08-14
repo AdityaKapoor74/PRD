16854f83ebed4b03a402c76609cccff42d922d57
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-15 02:23:12 +0530

    Update train_agent.py

--- Agent/MAPPO_Q_V/train_agent.py
+++ Agent/MAPPO_Q_V/train_agent.py
@@ -298,8 +298,8 @@ if __name__ == '__main__':
 				"value_clip": 0.2,
 				"enable_hard_attention": False,
 				"num_heads": 4,
-				"critic_weight_entropy_pen": 0.0,
-				"critic_score_regularizer": 1e-3,
+				"critic_weight_entropy_pen": 1e-2,
+				"critic_score_regularizer": 0.0,
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
 				"norm_returns": False,
 				
