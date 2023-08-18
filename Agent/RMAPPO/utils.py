812e767a5141543574791fc54fa99df2a5299794
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-08-18 17:37:41 +0530

    Update utils.py

--- Agent/RMAPPO/utils.py
+++ Agent/RMAPPO/utils.py
@@ -153,9 +153,9 @@ class RolloutBuffer:
 
 		if self.time_step < self.max_time_steps-1:
 			self.time_step += 1
-		else:
-			self.episode_num += 1
-			self.time_step = 0
+		# else:
+		# 	self.episode_num += 1
+		# 	self.time_step = 0
 
 	def end_episode(self, t):
 		self.episode_length[self.episode_num] = t
