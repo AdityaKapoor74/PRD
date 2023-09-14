4af03aa5690920b4022422b43333e5db9055799c
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-09-14 15:44:07 +0530

    Update train_agent.py

--- Agent/RMAPPO/train_agent.py
+++ Agent/RMAPPO/train_agent.py
@@ -288,7 +288,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
 		env_name = "10m_vs_11m"
-		experiment_type = "prd_soft_advantage" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
+		experiment_type = "shared" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
 				# TRAINING
@@ -339,7 +339,7 @@ if __name__ == '__main__':
 				"attention_dropout_prob_v": 0.0,
 				"q_weight_decay": 0.0,
 				"v_weight_decay": 0.0,
-				"enable_grad_clip_critic": True,
+				"enable_grad_clip_critic": False,
 				"grad_clip_critic": 10.0,
 				"value_clip": 0.2,
 				"enable_hard_attention": False,
@@ -354,7 +354,7 @@ if __name__ == '__main__':
 
 				# ACTOR
 				"rnn_hidden_actor": 64,
-				"enable_grad_clip_actor": True,
+				"enable_grad_clip_actor": False,
 				"grad_clip_actor": 10.0,	
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
