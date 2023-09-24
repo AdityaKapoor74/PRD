d25d52b6bf8275c96bbaa327f1a839d7e19ebbbc
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-09-24 18:53:29 +0530

    Update train_agent.py

--- Agent/RMAPPO/train_agent.py
+++ Agent/RMAPPO/train_agent.py
@@ -396,8 +396,8 @@ if __name__ == '__main__':
 				"policy_clip": 0.05,
 				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 2e-3, #8e-3
-				"entropy_pen_final": 2e-3,
+				"entropy_pen": 4e-3, #8e-3
+				"entropy_pen_final": 4e-3,
 				"entropy_pen_steps": 5000,
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.1, #0.1,
