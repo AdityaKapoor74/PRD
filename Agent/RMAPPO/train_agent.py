f04ec91fbafec38cffffffa3d6d2785dfcb6c7de
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-09-24 13:42:32 +0530

    Update train_agent.py

--- Agent/RMAPPO/train_agent.py
+++ Agent/RMAPPO/train_agent.py
@@ -386,7 +386,7 @@ if __name__ == '__main__':
 				"critic_weight_entropy_pen_steps": 100, # number of updates
 				"critic_score_regularizer": 0.0,
 				"lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
-				"norm_returns": False,
+				"norm_returns": True,
 				
 
 				# ACTOR
@@ -396,8 +396,8 @@ if __name__ == '__main__':
 				"policy_clip": 0.05,
 				"policy_lr": 1e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 6e-3, #8e-3
-				"entropy_pen_final": 6e-3,
+				"entropy_pen": 1e-2, #8e-3
+				"entropy_pen_final": 1e-2,
 				"entropy_pen_steps": 5000,
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.1, #0.1,
