5b9569d0362049c3653ff4c9db8945cb9735d925
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-09-25 10:54:23 +0530

    Update train_agent.py

--- Agent/RMAPPO/train_agent.py
+++ Agent/RMAPPO/train_agent.py
@@ -324,7 +324,7 @@ if __name__ == '__main__':
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
 		env_name = "10m_vs_11m"
-		experiment_type = "prd_soft_advantage" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
+		experiment_type = "shared" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
 				# TRAINING
@@ -370,8 +370,8 @@ if __name__ == '__main__':
 				"rnn_hidden_v": 64,				
 				"q_value_lr": 5e-4, #1e-3
 				"v_value_lr": 5e-4, #1e-3
-				"temperature_v": 1.0,
-				"temperature_q": 1.0,
+				"temperature_v": 0.0,
+				"temperature_q": 0.0,
 				"attention_dropout_prob_q": 0.0,
 				"attention_dropout_prob_v": 0.0,
 				"q_weight_decay": 0.0,
