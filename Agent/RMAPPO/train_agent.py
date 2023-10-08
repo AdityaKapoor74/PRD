a51517a6545a197d24b2696a3fccaf791304d10b
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-10-08 19:30:34 +0530

    Update train_agent.py

--- Agent/RMAPPO/train_agent.py
+++ Agent/RMAPPO/train_agent.py
@@ -386,7 +386,7 @@ if __name__ == '__main__':
 				"top_k": 0,
 				"norm_adv": False,
 
-				"network_update_interval_q": 1,
+				"network_update_interval_q": 200,
 				"network_update_interval_v": 1,
 			}
 
