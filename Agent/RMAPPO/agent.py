d2013f5dbee668e14e70b07b650d1ac36a8649fd
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-10-09 03:23:36 +0530

    Update agent.py

--- Agent/RMAPPO/agent.py
+++ Agent/RMAPPO/agent.py
@@ -602,7 +602,7 @@ class PPOAgent:
 			timesteps = old_states_critic_allies.shape[1]
 			shape = (self.update_ppo_agent, timesteps, self.num_agents)
 			target_V_values = self.nstep_returns(rewards=rewards.view(*shape).to(self.device), mask=masks.view(*shape).to(self.device), next_mask=next_mask.view(self.update_ppo_agent, self.num_agents).to(self.device), values=Q_values_old.view(*shape), next_values=next_Q_values_old.view(self.update_ppo_agent, self.num_agents), nsteps=5).view(-1, self.num_agents)
-			target_V_values = torch.sum(target_V_values.unsqueeze(-2).repeat(1, self.num_agents, 1) * torch.transpose(torch.mean(weights_prd_old.cpu(), dim=1), -1, -2), dim=-1)
+			target_V_values = torch.sum(target_V_values.unsqueeze(-2).repeat(1, self.num_agents, 1) * torch.transpose(torch.mean(weights_prd_old, dim=1), -1, -2), dim=-1)
 
 			advantage = (target_V_values - Values_old).detach()
 			
