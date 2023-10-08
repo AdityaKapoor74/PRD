c2667eba01947f417c360f5ae954f43fdec3f1fb
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-10-08 17:55:31 +0530

    Update train_agent.py

--- Agent/RMAPPO/train_agent.py
+++ Agent/RMAPPO/train_agent.py
@@ -371,7 +371,7 @@ if __name__ == '__main__':
 				# ACTOR
 				"rnn_hidden_actor": 64,
 				"enable_grad_clip_actor": True,
-				"grad_clip_actor": 0.3,
+				"grad_clip_actor": 0.2,
 				"policy_clip": 0.05,
 				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
