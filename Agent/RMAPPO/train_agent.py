e6daa624d74b3e6669cc7a21a6602a1acb2fbf24
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-10-09 16:26:11 +0530

    Update train_agent.py

--- Agent/RMAPPO/train_agent.py
+++ Agent/RMAPPO/train_agent.py
@@ -298,8 +298,8 @@ if __name__ == '__main__':
 	for i in range(1,4):
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
-		env_name = "bane_vs_bane"
-		experiment_type = "prd_above_threshold" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
+		env_name = "5m_vs_6m"
+		experiment_type = "prd_soft_advantage" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
 				# TRAINING
@@ -352,10 +352,10 @@ if __name__ == '__main__':
 				"attention_dropout_prob_v": 0.0,
 				"q_weight_decay": 0.0,
 				"v_weight_decay": 0.0,
-				"enable_grad_clip_critic_v": False,
-				"grad_clip_critic_v": 2.0,
-				"enable_grad_clip_critic_q": False,
-				"grad_clip_critic_q": 0.5,
+				"enable_grad_clip_critic_v": True,
+				"grad_clip_critic_v": 0.5,
+				"enable_grad_clip_critic_q": True,
+				"grad_clip_critic_q": 10.0,
 				"value_clip": 0.05,
 				"enable_hard_attention": False,
 				"num_heads": 1,
@@ -370,7 +370,7 @@ if __name__ == '__main__':
 
 				# ACTOR
 				"rnn_hidden_actor": 64,
-				"enable_grad_clip_actor": False,
+				"enable_grad_clip_actor": True,
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.05,
 				"policy_lr": 5e-4, #prd 1e-4
