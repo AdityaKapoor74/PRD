0ace17dd4c974804637f657c17ed31fedcc5d43f
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2023-10-25 14:02:53 +0530

    Update train_agent.py

--- Agent/RMAPPO_v2/train_agent.py
+++ Agent/RMAPPO_v2/train_agent.py
@@ -361,7 +361,7 @@ if __name__ == '__main__':
 				"v_weight_decay": 0.0,
 				"enable_grad_clip_critic_v": True,
 				"grad_clip_critic_v": 0.5,
-				"enable_grad_clip_critic_q": True,
+				"enable_grad_clip_critic_qs": True,
 				"grad_clip_critic_q": 0.5,
 				"value_clip": 0.2,
 				"enable_hard_attention": False,
@@ -386,8 +386,8 @@ if __name__ == '__main__':
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 1e-2, #8e-3
-				"entropy_pen_final": 1e-2,
+				"entropy_pen": 2e-2, #8e-3
+				"entropy_pen_final": 2e-2,
 				"entropy_pen_steps": 20000,
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0, #0.043, 0.1
