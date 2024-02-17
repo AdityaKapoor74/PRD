20e82ff9be4bbac8d59af8b0dde20d31d3c0ab96
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-17 13:01:05 +0530

    Update and rename utils.py to utils_old.py

--- Agent/RMAPPO_global_rew_PRD/utils_old.py
+++ Agent/RMAPPO_global_rew_PRD/utils_old.py
@@ -2,7 +2,7 @@ import torch
 from torch import Tensor
 import numpy as np
 
-from model import ValueNorm, RunningMeanStd
+from model_old import ValueNorm, RunningMeanStd
 
 
 def gumbel_sigmoid(logits: Tensor, tau: float = 1, hard: bool = False, threshold: float = 0.5) -> Tensor:
