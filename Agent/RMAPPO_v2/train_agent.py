0e13c4998cbd8adb55e4c654e92e191d4128ba8e
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-20 01:45:57 +0530

    Update train_agent.py

--- Agent/RMAPPO_v2/train_agent.py
+++ Agent/RMAPPO_v2/train_agent.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
 	for i in range(1,4):
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
-		env_name = "5m_vs_6m"
+		env_name = "10m_vs_11m"
 		experiment_type = "prd_top_k" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
@@ -399,7 +399,7 @@ if __name__ == '__main__':
 				"threshold_min": 0.0, 
 				"threshold_max": 0.25, #0.12
 				"steps_to_take": 1000,
-				"top_k": 3, # 3 (5m_vs_6m), 5 (10m_vs_11m), 4 (3s5z)
+				"top_k": 5, # 3 (5m_vs_6m), 5 (10m_vs_11m), 4 (3s5z)
 				"norm_adv": True,
 
 				"soft_update_q": False,
