b1c29dfb83c851459e5bec6d41d8cd3ab60226b1
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-20 11:29:59 +0530

    Update train_agent.py

--- Agent/RMAPPO_v2/train_agent.py
+++ Agent/RMAPPO_v2/train_agent.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
 	for i in range(1,4):
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
-		env_name = "10m_vs_11m"
+		env_name = "3s5z"
 		experiment_type = "shared" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
