204696e01d032a2bb0336ff016f30e8dce788efa
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-21 10:05:08 +0530

    Update train_agent.py

--- Agent/RMAPPO_v2/train_agent.py
+++ Agent/RMAPPO_v2/train_agent.py
@@ -389,17 +389,17 @@ if __name__ == '__main__':
 				"enable_grad_clip_actor": True,
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.2,
-				"policy_lr": 5e-4, #prd 1e-4
+				"policy_lr": 1e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 2e-3, #8e-3
-				"entropy_pen_final": 2e-3,
+				"entropy_pen": 1e-3, #8e-3
+				"entropy_pen_final": 1e-3,
 				"entropy_pen_steps": 20000,
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0, # 0.4 (5m_vs_6m), 0.25 (10m_vs_11m), 0.25 (3s5z)
 				"threshold_min": 0.0, 
-				"threshold_max": 0.25, #0.12
+				"threshold_max": 0.3,
 				"steps_to_take": 1000,
-				"top_k": 4, # 3 (5m_vs_6m), 5 (10m_vs_11m), 4 (3s5z)
+				"top_k": 0, # 3 (5m_vs_6m), 5 (10m_vs_11m), 4 (3s5z)
 				"norm_adv": True,
 
 				"soft_update_q": False,
