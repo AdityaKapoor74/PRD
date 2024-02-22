256af5e38e64c8c60662c30846a05df5b39c514d
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-23 01:13:56 +0530

    Update train_agent.py

--- Agent/RMAPPO_v2/train_agent.py
+++ Agent/RMAPPO_v2/train_agent.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
 	for i in range(1,4):
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
-		env_name = "5m_vs_6m"
+		env_name = "10m_vs_11m"
 		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
@@ -397,7 +397,7 @@ if __name__ == '__main__':
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0, # 0.4 (5m_vs_6m), 0.25 (10m_vs_11m), 0.25 (3s5z)
 				"threshold_min": 0.0, 
-				"threshold_max": 0.3,
+				"threshold_max": 0.16,
 				"steps_to_take": 1000,
 				"top_k": 0, # 3 (5m_vs_6m), 5 (10m_vs_11m), 4 (3s5z)
 				"norm_adv": True,
