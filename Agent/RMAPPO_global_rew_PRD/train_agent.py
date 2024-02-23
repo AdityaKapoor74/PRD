4440ae32324151b8f3cbd76e638535b22daf98fc
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-23 22:02:54 +0530

    Update train_agent.py

--- Agent/RMAPPO_global_rew_PRD/train_agent.py
+++ Agent/RMAPPO_global_rew_PRD/train_agent.py
@@ -403,13 +403,13 @@ if __name__ == '__main__':
 				"rnn_hidden_q": 64,
 				"rnn_hidden_v": 64,				
 				"q_value_lr": 5e-4, #1e-3
-				"v_value_lr": 1e-4, #1e-3
+				"v_value_lr": 5e-4, #1e-3
 				"temperature_v": 1.0,
 				"temperature_q": 1.0,
 				"attention_dropout_prob_q": 0.0,
 				"attention_dropout_prob_v": 0.0,
-				"q_weight_decay": 0.0,
-				"v_weight_decay": 0.0,
+				"q_weight_decay": 5e-4,
+				"v_weight_decay": 5e-4,
 				"enable_grad_clip_critic_v": True,
 				"grad_clip_critic_v": 0.5,
 				"enable_grad_clip_critic_q": True,
@@ -442,7 +442,7 @@ if __name__ == '__main__':
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
-				"policy_weight_decay": 0.0,
+				"policy_weight_decay": 5e-4,
 				"entropy_pen": 5e-4, #8e-3
 				"entropy_pen_final": 5e-4,
 				"entropy_pen_steps": 20000,
