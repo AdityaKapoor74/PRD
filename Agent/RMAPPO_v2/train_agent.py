a7cd16cdbd71cf78d18c55eadcad3300c38f9850
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-23 17:26:14 +0530

    Update train_agent.py

--- Agent/RMAPPO_v2/train_agent.py
+++ Agent/RMAPPO_v2/train_agent.py
@@ -306,8 +306,8 @@ if __name__ == '__main__':
 	for i in range(1,4):
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
-		env_name = "3s5z"
-		experiment_type = "prd_above_threshold_decay" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
+		env_name = "5m_vs_6m"
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
 				# TRAINING
@@ -389,16 +389,16 @@ if __name__ == '__main__':
 				"enable_grad_clip_actor": True,
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.2,
-				"policy_lr": 1e-3, #prd 1e-4
+				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 1e-3, #8e-3
-				"entropy_pen_final": 1e-3,
+				"entropy_pen": 1e-2, #8e-3
+				"entropy_pen_final": 1e-2,
 				"entropy_pen_steps": 20000,
 				"gae_lambda": 0.95,
-				"select_above_threshold": 0.14, # 0.4 (5m_vs_6m), 0.25 (10m_vs_11m), 0.25 (3s5z)
+				"select_above_threshold": 0.0, # 0.4 (5m_vs_6m), 0.25 (10m_vs_11m), 0.25 (3s5z)
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0,
-				"steps_to_take": 5000,
+				"threshold_max": 0.25,
+				"steps_to_take": 1000,
 				"top_k": 0, # 3 (5m_vs_6m), 5 (10m_vs_11m), 4 (3s5z)
 				"norm_adv": True,
 
