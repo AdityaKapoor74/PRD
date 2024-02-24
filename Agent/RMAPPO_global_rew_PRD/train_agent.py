f2d1c555c17d444be21d151e603ce9d9efa30fb0
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-24 10:56:21 +0530

    Update train_agent.py

--- Agent/RMAPPO_global_rew_PRD/train_agent.py
+++ Agent/RMAPPO_global_rew_PRD/train_agent.py
@@ -360,7 +360,7 @@ if __name__ == '__main__':
 	for i in range(1,4):
 		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
-		env_name = "5m_vs_6m"
+		env_name = "10m_vs_11m"
 		experiment_type = "prd_soft_advantage_global" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage
 
 		dictionary = {
