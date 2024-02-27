9653782edadf004fa4ef4232599aee30b8edc6f4
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-02-27 11:22:57 +0530

    Update model.py

--- Agent/MAPPO_variants/model.py
+++ Agent/MAPPO_variants/model.py
@@ -774,7 +774,8 @@ class Q_network(nn.Module):
 
 		# SOFT ATTENTION
 		score = torch.matmul(query_obs,(key_obs).transpose(-2,-1))/(self.d_k_agents//self.num_heads)**(1/2) # Batch_size, Num Heads, Num agents, Num Agents
-		attention_masks = self.get_attention_masks(agent_masks) + (1-hard_attention_weights)*self.mask_value
+		attention_masks = self.get_attention_masks(agent_masks).reshape(batch*timesteps, num_agents, num_agents).unsqueeze(1).repeat(1, self.num_heads, 1, 1)
+		attention_masks = attention_masks + (1-hard_attention_weights)*self.mask_value
 		# score = score + attention_masks.reshape(*score.shape).to(score.device)
 		# max_score = torch.max(score, dim=-1, keepdim=True).values
 		# score_stable = score - max_score
@@ -1027,13 +1028,13 @@ class V_network(nn.Module):
 			
 		# SOFT ATTENTION
 		score = torch.matmul(query_obs,(key_obs).transpose(-2,-1))/(self.d_k_agents//self.num_heads)**(1/2) # Batch_size, Num Heads, Num agents, Num Agents
-		attention_masks = self.get_attention_masks(agent_masks)
+		attention_masks = self.get_attention_masks(agent_masks).reshape(batch*timesteps, num_agents, num_agents).unsqueeze(1).repeat(1, self.num_heads, 1, 1)
+		attention_masks = attention_masks + (1-hard_attention_weights)*self.mask_value
 		# score = score + attention_masks.reshape(*score.shape).to(score.device)
 		# max_score = torch.max(score, dim=-1, keepdim=True).values
 		# score_stable = score - max_score
-		weights = F.softmax((score/(torch.max(score, dim=-1).values-torch.min(score, dim=-1).values+1e-5).detach().unsqueeze(-1)) + attention_masks.reshape(*score.shape).to(score.device), dim=-1) #* hard_attention_weights.unsqueeze(1).permute(0, 1, 2, 4, 3) # Batch_size, Num Heads, Num agents, 1, Num Agents - 1
+		weights = F.softmax((score/(torch.max(score, dim=-1).values-torch.min(score, dim=-1).values+1e-5).detach().unsqueeze(-1)) + attention_masks.reshape(*score.shape).to(score.device), dim=-1) # Batch_size, Num Heads, Num agents, 1, Num Agents - 1
 		# weights = F.softmax(score, dim=-1)
-		weights = weights * hard_attention_weights # Batch_size, Num Heads, Num agents, Num Agents
 
 		final_weights = weights.clone()
 		for i in range(self.num_agents):
