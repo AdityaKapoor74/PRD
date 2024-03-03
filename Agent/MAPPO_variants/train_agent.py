859122c5ca7e9ac3cb61c56ce27b26824a34ae71
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-03-03 09:47:05 +0530

    Update train_agent.py

--- Agent/MAPPO_variants/train_agent.py
+++ Agent/MAPPO_variants/train_agent.py
@@ -564,7 +564,7 @@ if __name__ == '__main__':
 			grid_size = 12
 			fully_coop = False
 		env_name = "5m_vs_6m" # 5m_vs_6m/ 10m_vs_11m/ 3s5z/ crossing_team_greedy/ pressureplate-linear-6p-v0/ pursuit_v4/ "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "")
-		experiment_type = "HAPPO" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage prd_soft_advantage_global, HAPPO
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage prd_soft_advantage_global, HAPPO
 
 		dictionary = {
 				# TRAINING
@@ -623,9 +623,9 @@ if __name__ == '__main__':
 				"q_weight_decay": 0.0,
 				"v_weight_decay": 0.0,
 				"enable_grad_clip_critic_v": True,
-				"grad_clip_critic_v": 10.0,
+				"grad_clip_critic_v": 0.5,
 				"enable_grad_clip_critic_q": True,
-				"grad_clip_critic_q": 10.0,
+				"grad_clip_critic_q": 0.5,
 				"value_clip": 0.2,
 				"enable_hard_attention": False,
 				"num_heads": 1,
@@ -650,17 +650,17 @@ if __name__ == '__main__':
 				"rnn_num_layers_actor": 1,
 				"rnn_hidden_actor": 64,
 				"enable_grad_clip_actor": True,
-				"grad_clip_actor": 10.0,
+				"grad_clip_actor": 0.5,
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 1e-2, #8e-3
-				"entropy_pen_final": 1e-2,
+				"entropy_pen": 1e-3, #8e-3
+				"entropy_pen_final": 1e-3,
 				"entropy_pen_steps": 20000,
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0, 
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0, 
+				"threshold_max": 0.2, 
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": True,
