d064505f5fef5c2b91462c5ca0e90cf71ec05ac3
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-03-04 16:59:01 +0530

    Update train_agent.py

--- Agent/MAPPO_variants/train_agent.py
+++ Agent/MAPPO_variants/train_agent.py
@@ -652,10 +652,10 @@ if __name__ == '__main__':
 				"enable_grad_clip_actor": True,
 				"grad_clip_actor": 10.0,
 				"policy_clip": 0.2,
-				"policy_lr": 2e-4, #prd 1e-4
+				"policy_lr": 1e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 5e-2, #8e-3
-				"entropy_pen_final": 5e-2,
+				"entropy_pen": 2e-2, #8e-3
+				"entropy_pen_final": 2e-2,
 				"entropy_pen_steps": 20000,
 				"gae_lambda": 0.95,
 				"select_above_threshold": 0.0, 
