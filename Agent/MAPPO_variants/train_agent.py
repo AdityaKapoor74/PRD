1b058cb694676a6f40a93185bbb76613bf11f418
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-03-07 13:09:20 +0530

    Update train_agent.py

--- Agent/MAPPO_variants/train_agent.py
+++ Agent/MAPPO_variants/train_agent.py
@@ -563,8 +563,8 @@ if __name__ == '__main__':
 			num_food = 9
 			grid_size = 12
 			fully_coop = False
-		env_name = "3s5z" # 5m_vs_6m/ 10m_vs_11m/ 3s5z/ crossing_team_greedy/ pressureplate-linear-6p-v0/ pursuit_v4/ "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "")
-		experiment_type = "HAPPO" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage prd_soft_advantage_global, HAPPO
+		env_name = "10m_vs_11m" # 5m_vs_6m/ 10m_vs_11m/ 3s5z/ crossing_team_greedy/ pressureplate-linear-6p-v0/ pursuit_v4/ "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "")
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage prd_soft_advantage_global, HAPPO
 
 		dictionary = {
 				# TRAINING
@@ -654,13 +654,13 @@ if __name__ == '__main__':
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
-				"entropy_pen": 1e-2, #8e-3
-				"entropy_pen_final": 1e-2,
+				"entropy_pen": 1e-3, #8e-3
+				"entropy_pen_final": 1e-3,
 				"entropy_pen_steps": 20000,
 				"gae_lambda": 0.95,
-				"select_above_threshold": 0.15, 
+				"select_above_threshold": 0.0, 
 				"threshold_min": 0.0, 
-				"threshold_max": 0.0, 
+				"threshold_max": 0.2, 
 				"steps_to_take": 1000,
 				"top_k": 0,
 				"norm_adv": True,
