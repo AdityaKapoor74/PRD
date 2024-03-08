a2bc08957eef30896bef4754dcfa593ccbddbd66
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-03-08 16:24:31 +0530

    Update main_ppo.py

--- Agent/PRD_old/main_ppo.py
+++ Agent/PRD_old/main_ppo.py
@@ -1,20 +1,36 @@
 from mappo import MAPPO
 import torch
-import gym
-import smaclite  # noqa
+
+
+def make_env(scenario_name, benchmark=False):
+	scenario = scenarios.load(scenario_name + ".py").Scenario()
+	world = scenario.make_world()
+	if benchmark:
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data, scenario.isFinished)
+	else:
+		env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, None, scenario.isFinished)
+	return env, scenario.actor_observation_shape, scenario.critic_observation_shape
 
 
 if __name__ == '__main__':
+	
 	RENDER = False
 	USE_CPP_RVO2 = False
+
+	torch.set_printoptions(profile="full")
+	torch.autograd.set_detect_anomaly(True)
 	
 	for i in range(1,4):
-		extension = "PRD_old_"+str(i)
+		extension = "MAPPO_"+str(i)
 		test_num = "StarCraft"
-		env_name = "5m_vs_6m"
-		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay
-
-		torch.autograd.set_detect_anomaly(True)
+		environment = "LBForaging" # StarCraft/ MPE/ PressurePlate/ PettingZoo/ LBForaging
+		if "LBForaging" in environment:
+			num_players = 6
+			num_food = 9
+			grid_size = 12
+			fully_coop = False
+		env_name = "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "") # 5m_vs_6m/ 10m_vs_11m/ 3s5z/ crossing_team_greedy/ pressureplate-linear-6p-v0/ pursuit_v4/ "Foraging-{0}x{0}-{1}p-{2}f{3}-v2".format(grid_size, num_players, num_food, "-coop" if fully_coop else "")
+		experiment_type = "prd_above_threshold_ascend" # shared, prd_above_threshold_ascend, prd_above_threshold, prd_top_k, prd_above_threshold_decay, prd_soft_advantage, prd_soft_advantage_global
 
 		dictionary = {
 				# TRAINING
@@ -27,6 +43,7 @@ if __name__ == '__main__':
 				"policy_eval_dir":'../../../tests/'+test_num+'/policy_eval/'+env_name+'_'+experiment_type+'_'+extension+'/',
 				"n_epochs": 5,
 				"update_ppo_agent": 10, # update ppo agent after every update_ppo_agent episodes
+				"environment": environment,
 				"test_num":test_num,
 				"extension":extension,
 				"gamma": 0.99,
@@ -40,8 +57,8 @@ if __name__ == '__main__':
 				"save_model_checkpoint": 1000,
 				"save_comet_ml_plot": True,
 				"learn":True,
-				"max_episodes": 20000,
-				"max_time_steps": 100,
+				"max_episodes": 15000,
+				"max_time_steps": 70,
 				"experiment_type": experiment_type,
 				"parallel_training": False,
 				"scheduler_need": False,
@@ -60,7 +77,7 @@ if __name__ == '__main__':
 				"attention_drop_prob": 0.0,
 				"value_lr": 5e-4, #1e-3
 				"value_weight_decay": 5e-4,
-				"enable_grad_clip_critic": False,
+				"enable_grad_clip_critic": True,
 				"grad_clip_critic": 10.0,
 				"value_clip": 0.2,
 				"enable_hard_attention": False,
@@ -70,15 +87,14 @@ if __name__ == '__main__':
 				"target_calc_style": "GAE", # GAE, TD_Lambda, N_steps
 				"n_steps": 5,
 				"td_lambda": 0.95, # 1 --> Monte Carlo; 0 --> TD(1)
-				"norm_returns": False,
-				"norm_returns_critic": True,
+				"norm_returns": True,
 				
 
 				# ACTOR
 				"data_chunk_length": 10,
 				"rnn_num_layers_actor": 1,
 				"rnn_hidden_actor_dim": 64,
-				"enable_grad_clip_actor": False,
+				"enable_grad_clip_actor": True,
 				"grad_clip_actor": 10.0,
 				"policy_clip": 0.2,
 				"policy_lr": 5e-4, #prd 1e-4
@@ -97,10 +113,70 @@ if __name__ == '__main__':
 
 		seeds = [42, 142, 242, 342, 442]
 		torch.manual_seed(seeds[dictionary["iteration"]-1])
-		env = gym.make(f"smaclite/{env_name}-v0", use_cpp_rvo2=USE_CPP_RVO2)
-		obs, info = env.reset(return_info=True)
-		dictionary["ally_observation"] = info["ally_states"].shape[1]+env.n_agents
-		dictionary["enemy_observation"] = info["enemy_states"].shape[1]
-		dictionary["local_observation"] = obs[0].shape[0]+env.n_agents
+		
+		if "StarCraft" in environment:
+			
+			import gym
+			import smaclite  # noqa
+			
+			env = gym.make(f"smaclite/{env_name}-v0", use_cpp_rvo2=USE_CPP_RVO2)
+			obs, info = env.reset(return_info=True)
+			dictionary["ally_observation"] = info["ally_states"].shape[1]+env.n_agents
+			dictionary["enemy_observation"] = info["enemy_states"].shape[1]+env.n_enemies
+			dictionary["local_observation"] = obs[0].shape[0]+env.n_agents+env.action_space[0].n
+			dictionary["num_agents"] = env.n_agents
+			dictionary["num_actions"] = env.action_space[0].n
+
+		elif "MPE" in environment:
+
+			from multiagent.environment import MultiAgentEnv
+			import multiagent.scenarios as scenarios
+
+			env, actor_observation_shape, critic_observation_shape = make_env(scenario_name=dictionary["env"], benchmark=False)
+			dictionary["ally_observation"] = critic_observation_shape
+			dictionary["local_observation"] = actor_observation_shape+env.action_space[0].n
+			dictionary["num_agents"] = env.n
+			dictionary["num_actions"] = env.action_space[0].n
+
+		elif "PressurePlate" in environment:
+
+			import pressureplate
+			import gym
+
+			env = gym.make(env_name)
+			dictionary["ally_observation"] = 133+env.n_agents
+			dictionary["local_observation"] = 133+5+env.n_agents
+			dictionary["num_agents"] = env.n_agents
+			dictionary["num_actions"] = 5
+
+		elif "PettingZoo" in environment:
+
+			from pettingzoo.sisl import pursuit_v4
+
+			num_agents = 8
+			num_actions = 5
+			obs_range = 7
+
+			env = pursuit_v4.parallel_env(max_cycles=dictionary["max_time_steps"], x_size=16, y_size=16, shared_reward=False, n_evaders=30,
+									n_pursuers=num_agents, obs_range=obs_range, n_catch=2, freeze_evaders=False, tag_reward=0.01,
+									catch_reward=5.0, urgency_reward=-0.1, surround=True, constraint_window=1.0)
+			
+			dictionary["ally_observation"] = obs_range*obs_range*3 + num_agents
+			dictionary["local_observation"] = obs_range*obs_range*3 + num_agents + num_actions
+			dictionary["num_agents"] = num_agents
+			dictionary["num_actions"] = num_actions
+
+		elif "LBForaging" in environment:
+
+			import lbforaging
+			import gym
+
+			env = gym.make(env_name, max_episode_steps=dictionary["max_time_steps"], penalty=0.0, normalize_reward=True)
+			dictionary["ally_observation"] = num_players*3 + num_food*3 + num_players
+			dictionary["local_observation"] = num_players*3 + num_food*3 + num_players + env.action_space[0].n
+			dictionary["num_agents"] = num_players
+			dictionary["num_actions"] = env.action_space[0].n
+
+
 		ma_controller = MAPPO(env,dictionary)
 		ma_controller.run()
