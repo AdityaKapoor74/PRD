355688e79266c47ef8e1c526079413ed5b3d937f
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-03-09 03:48:06 +0530

    Update main_coma.py

--- Agent/COMA/main_coma.py
+++ Agent/COMA/main_coma.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 				"critic_entropy_pen": 0.0,
 				"epsilon_start": 1.0,
 				"epsilon_end": 0.05,
-				"entropy_pen": 8e-3, #8e-3
+				"entropy_pen": 1e-3, #8e-3
 				"epsilon_episode_steps": 5000,
 				"target_critic_update": 200,
 				"gamma": 0.99,
