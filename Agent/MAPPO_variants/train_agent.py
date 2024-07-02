bcd8575e907b28e70ad9981faf5ed6730c260ce8
Aditya Kapoor <39992294+AdityaKapoor74@users.noreply.github.com>
2024-07-02 12:59:51 +0530

    Update train_agent.py

--- Agent/MAPPO_variants/train_agent.py
+++ Agent/MAPPO_variants/train_agent.py
@@ -576,8 +576,8 @@ if __name__ == '__main__':
 				"rnn_num_layers_v": 1,
 				"rnn_hidden_q": 64,
 				"rnn_hidden_v": 64,				
-				"q_value_lr": 1e-4, #1e-3
-				"v_value_lr": 1e-4, #1e-3
+				"q_value_lr": 5e-4, #1e-3
+				"v_value_lr": 5e-4, #1e-3
 				"temperature_v": 1.0,
 				"temperature_q": 1.0,
 				"attention_dropout_prob_q": 0.0,
@@ -617,7 +617,7 @@ if __name__ == '__main__':
 				"enable_grad_clip_actor": True,
 				"grad_clip_actor": 0.5,
 				"policy_clip": 0.2,
-				"policy_lr": 1e-4, #prd 1e-4
+				"policy_lr": 5e-4, #prd 1e-4
 				"policy_weight_decay": 0.0,
 				"entropy_pen": 1e-3, #8e-3
 				"entropy_pen_final": 1e-3,
